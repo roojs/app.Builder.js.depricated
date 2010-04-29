c8278691ba951faee7106fc8e5560f7515be17aa
Alan Knowles <alan@akbkhome.com>
2010-04-29 12:37:22 +0800

    Builder/RightGtkView.js

---
+++ Builder/RightGtkView.js
@@ -0,0 +1,17 @@
+//<script type="text/javascript">
+
+/**
+* we could basically use a GtkScrolled window?
+* 
+* GtkScrolled 
+*    -> Vbox
+*      => fake title bar (which acks as a proxy for the window element..
+*      => box which contains the elements ...
+* 
+
+
+*/
+
+
+
+
