50a4e65ef9a436075cfdb86cb544fc7a87052a92
Alan Knowles <alan@akbkhome.com>
2010-04-29 18:29:38 +0800

    Builder/LeftPanelPopup.js

--- Builder/LeftPanelPopup.js
+++ Builder/LeftPanelPopup.js
@@ -11,48 +11,46 @@ console = imports.console;
 LeftPanelPopup = new XObject({
     
         
-        xtype : Gtk.Menu,
-        
-         
-        items :  [
-            {
-                
-                
-                xtype : Gtk.MenuItem',
-                pack : [ 'append' ],
-                label : 'Delete Property / Event',
-                listeners : {
-                    activate : function () {
-                        LeftPanel.get('model').deleteSelected();
-                    }
-                }
-            },
-        
-          {
-                
-                
-                xtype : Gtk.MenuItem,
-                pack : [ 'append' ],
-                label : 'Change Property to Javascript Value',
-                listeners : {
-                    activate : function () {
-                       LeftPanel.get('model').setSelectedToJS();
-                    }
-                }
-            },
-            {
-                
-                
-                xtype : Gtk.MenuItem,
-                pack : [ 'append' ],
-                label : 'Change Property to String (or native) Value',
-                listeners : {
-                    activate : function () {
-                        LeftPanel.get('model').setSelectedToNoJS();
-                    }
-                }
-            },
-        ]
-    }
+    xtype : Gtk.Menu,
     
-);
+     
+    items :  [
+        {
+            
+            
+            xtype : Gtk.MenuItem',
+            pack : [ 'append' ],
+            label : 'Delete Property / Event',
+            listeners : {
+                activate : function () {
+                    LeftPanel.get('model').deleteSelected();
+                }
+            }
+        },
+    
+      {
+            
+            
+            xtype : Gtk.MenuItem,
+            pack : [ 'append' ],
+            label : 'Change Property to Javascript Value',
+            listeners : {
+                activate : function () {
+                   LeftPanel.get('model').setSelectedToJS();
+                }
+            }
+        },
+        {
+            
+            
+            xtype : Gtk.MenuItem,
+            pack : [ 'append' ],
+            label : 'Change Property to String (or native) Value',
+            listeners : {
+                activate : function () {
+                    LeftPanel.get('model').setSelectedToNoJS();
+                }
+            }
+        },
+    ]
+});
