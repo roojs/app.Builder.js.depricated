e85a2f62dd594a8669632efec938b83a0d475682
Alan Knowles <alan@akbkhome.com>
2010-04-30 12:03:59 +0800

    Builder/Provider/Palete/Base.js

---
+++ Builder/Provider/Palete/Base.js
@@ -0,0 +1,93 @@
+//<Script type="text/javascript">
+
+Gio = imports.gi.Gio;
+
+console = imports.console;
+XObject = imports.XObject.XObject;
+
+
+// Palete Provider..
+Base = XObject.define(
+    function(cfg) {
+    
+        XObject.extend(this, cfg);
+        // various loader methods..
+   
+    },
+    Object, 
+    {
+        
+        
+        map : false, // array of mappings   left: [] , right : []
+        
+        items : false, // the tree of nodes.
+        
+        
+        /**
+         * gather a  list of potentional objects that can be added..
+         * 
+         */
+        gatherList: function (existing) {
+            existing = existing || [];
+            existing.push('*top'); // always have top
+            var ret  = []; 
+            
+            function addRight(right) {
+                right.forEach(function(r) {
+                    if (ret.indexOf(r) > -1) {
+                        return;
+                    }
+                    ret.push(r);
+                });
+            }
+            
+            this.map.forEach(function(m) {
+                Roo.each(m.left, function(left) {
+                    var l = left.replace(/:.*$/, '');
+                    if (existing.indexOf(l) > -1) {
+                        addRight(m.right);
+                        return true; // no more needed..
+                    }
+                });
+                
+            });
+            return ret;
+            
+            
+            
+        },
+        
+        getDropList : function(rval)
+        {
+            
+            var ret = [];
+            this.map.forEach( function(m) {
+                if (m.right.indexOf(rval) > -1) {
+                    m.left.forEach(function(l) {
+                        if (ret.indexOf(l) > -1) {
+                            return;
+                        }
+                        ret.push(l)
+                    });
+                }
+                
+            });
+            console.log("DROP LIST:");
+            console.dump(ret);
+            return ret;
+            
+        },
+        
+        
+        confirmCanAdd: function(parent, child) {
+            // confirms that one obj can be added to another.
+            // returns true, for items, or list of properties that can hold it..
+            return true;
+            
+        }
+        
+    }
+);
+
+
+
