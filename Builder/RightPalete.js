df7bb6ee82364fb937f69a7e94cacc16a1ef3cf2
Alan Knowles <alan@akbkhome.com>
2010-05-04 12:14:02 +0800

    Builder/RightPalete.js

--- Builder/RightPalete.js
+++ Builder/RightPalete.js
@@ -178,12 +178,12 @@ RightPalete = new XObject({
                                             Gdk.DragAction.COPY         /* what to do with data after dropped */
                                     );
                                     
-                                    //print("ATOM:" + LeftTree.atoms["STRING"]);
-                                    //targets = new Gtk.TargetList();
-                                    //targets.add( LeftTree.atoms["STRING"], 0, 0);
-                                    //Gtk.drag_source_set_target_list(this.el, targets);
+                                    print("RP: TARGET:" + LeftTree.atoms["STRING"]);
+                                    targets = new Gtk.TargetList();
+                                    targets.add( LeftTree.atoms["STRING"], 0, 0);
+                                    Gtk.drag_source_set_target_list(this.el, targets);
                                     //if you want to allow text to be output elsewhere..
-                                    Gtk.drag_source_add_text_targets(this.el);
+                                    //Gtk.drag_source_add_text_targets(this.el);
                                     return true; 
                                 },  
                                 listeners : {
