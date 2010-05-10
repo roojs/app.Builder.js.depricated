ddb11de82d6c97bb45897cca62f77e4bbec7a838
Alan Knowles <alan@akbkhome.com>
2010-05-10 18:52:19 +0800

    Builder/Provider/ProjectManager.js

--- Builder/Provider/ProjectManager.js
+++ Builder/Provider/ProjectManager.js
@@ -164,7 +164,7 @@ ProjectManager =  new Observable({
         if (typeof(ProjectManager[type]) != 'undefined') {
             return ProjectManager[type];
         }
-        ProjectManager[type] =  new imports.Builder.Provider.Palete[type][xtype]();
+        ProjectManager[type] =  new imports.Builder.Provider.Palete[type][type]();
         return ProjectManager[type];
     }
 
