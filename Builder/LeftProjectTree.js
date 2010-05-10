026da9b50e49bee7ddc8d8d51a04ce8658ac0298
Alan Knowles <alan@akbkhome.com>
2010-05-10 19:50:12 +0800

    Builder/LeftProjectTree.js

--- Builder/LeftProjectTree.js
+++ Builder/LeftProjectTree.js
@@ -434,10 +434,10 @@ LeftProjectTree = new XObject({
                                     LeftProjectTree.get('view').el.expand_all();
                                     // needs more thought!!??
                                     var Window = imports.Builder.Window.Window;
-                                    print("PAGES: " + Window.get('notebook').el.get_n_pages());
-                                    Window.get('notebook').el.set_current_page(pr.xtype == 'Roo' ? 0 : -1);
                                     
-                                    print ("SET notebook to " + pr.xtype);
+                                    Window.get('view-notebook').el.set_current_page(pr.xtype == 'Roo' ? 0 : -1);
+                                    
+                                    
                                     
                                 },
                                 
