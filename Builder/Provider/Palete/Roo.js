e9d129090460f34f1de06ae186dac5ed2bc9b77e
Alan Knowles <alan@akbkhome.com>
2010-05-10 19:15:36 +0800

    Builder/Provider/Palete/Roo.js

--- Builder/Provider/Palete/Roo.js
+++ Builder/Provider/Palete/Roo.js
@@ -6,7 +6,7 @@ console = imports.console;
 XObject = imports.XObject.XObject;
 
 Base = imports.Builder.Provider.Palete.Base.Base;
-
+File = imports.File.File;
 //----------------------- our roo verison
 
 
@@ -27,63 +27,53 @@ Roo = XObject.define(
     
      
         load: function ( o ) {
-           Seed.print(__script_path__ +'/RooUsage.txt');
-             var file = Gio.file_new_for_path(__script_path__ +'/../RooUsage.txt');
-                                        
-             var _this = this;                        
+            
              
-                                        
-            file.read_async(0, null, function(source,result) {
-                var stream = source.read_finish(result)
-                var dstream = new Gio.DataInputStream.c_new(stream);
-              
-                var data =  dstream.read_until("")
-                data  = data.split(/\n/g);
-                var state = 0;
-                var cfg = [];
-                var left = [];
-                var right = [];
-                
-                data.forEach( function(d) {
-                    if (!d.length || d.match(/^\s+$/) || d.match(/^\//)) { //empty
-                        return;
-                    }
-                    if (d.match(/^left:/)) { 
-                        state = 1;
-                        if (left.length ){
-                            
-                            cfg.push({
-                                left : left,
-                                right: right
-                            });
-                            }
-                        left = [];
-                        right = [];
-                        return;
-                    }
-                    if (d.match(/^right:/)) { 
-                        state = 2;
-                        return;
-                    }
-                    if (state == 1) {
-                        left.push(d.replace(/\s+/g, ''));
-                        return;
-                    }
-                    right.push(d.replace(/\s+/g, ''));
-                    //Seed.quit();
-                   
-                }); 
-                if (left.length ){
-                            
-                    cfg.push({
-                        left : left,
-                        right: right
-                    });
+         
+            var data  = File.read(__script_path__ +'/../RooUsage.txt').data.split(/\n/g);
+            var state = 0;
+            var cfg = [];
+            var left = [];
+            var right = [];
+            
+            data.forEach( function(d) {
+                if (!d.length || d.match(/^\s+$/) || d.match(/^\//)) { //empty
+                    return;
                 }
-                _this.map = cfg;
-                if (o && o.success) o.success.apply(o.scope || _this, [_this]);
-                //console.dump(cfg);
-            });
+                if (d.match(/^left:/)) { 
+                    state = 1;
+                    if (left.length ){
+                        
+                        cfg.push({
+                            left : left,
+                            right: right
+                        });
+                        }
+                    left = [];
+                    right = [];
+                    return;
+                }
+                if (d.match(/^right:/)) { 
+                    state = 2;
+                    return;
+                }
+                if (state == 1) {
+                    left.push(d.replace(/\s+/g, ''));
+                    return;
+                }
+                right.push(d.replace(/\s+/g, ''));
+                //Seed.quit();
+               
+            }); 
+            if (left.length ){
+                        
+                cfg.push({
+                    left : left,
+                    right: right
+                });
+            }
+            this.map = cfg;
+            
         }
         
          
