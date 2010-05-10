080535d6558c04b4198522393131a5d8e7b819ac
Alan Knowles <alan@akbkhome.com>
2010-05-10 14:45:13 +0800

    Builder/RightGtkView.js

--- Builder/RightGtkView.js
+++ Builder/RightGtkView.js
@@ -8,9 +8,12 @@
 *      => fake title bar (which acks as a proxy for the window element..
 *      => box which contains the elements ...
 * 
-
-
+... gtk_offscreen_window_get_pixbuf 
+* - gtk_offscreen_window_get_pixmap 
+*   gtk_offscreen_window_new - gtk_paint_spinner ...
+could be rendered into a gtk.image ??
 */
+
 
 
 
