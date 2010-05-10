b1697022fb3d8482d44c5dec2867071b40874eb0
Alan Knowles <alan@akbkhome.com>
2010-05-10 19:32:52 +0800

    Builder/Window.js

--- Builder/Window.js
+++ Builder/Window.js
@@ -122,6 +122,7 @@ Window = new XObject({
                                             items : [
                                             
                                                 {
+                                                    id : 'notebook',
                                                     xtype : Gtk.Notebook,
                                                     show_tabs : false,
                                                     tab_border : 0,
