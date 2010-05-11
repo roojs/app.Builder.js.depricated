e8a771e9da0f26916b8c4abff6e9a6aa944acbcd
Alan Knowles <alan@akbkhome.com>
2010-05-11 10:39:23 +0800

    Builder/Provider/Palete/Gtk.js

--- Builder/Provider/Palete/Gtk.js
+++ Builder/Provider/Palete/Gtk.js
@@ -25,56 +25,57 @@ Gtk = XObject.define(
     },
     Base,
     {
-    
-     
-    
-        var data = File.read(__script_path__ +'/../GtkUsage.txt');
-        print(data);
-        data  = data.split(/\n/g);
-        var state = 0;
-        var cfg = [];
-        var left = [];
-        var right = [];
+        load: function () {
+                
+         
         
-        data.forEach( function(d) {
-            if (!d.length || d.match(/^\s+$/) || d.match(/^\//)) { //empty
-                return;
+            var data = File.read(__script_path__ +'/../GtkUsage.txt');
+            print(data);
+            data  = data.split(/\n/g);
+            var state = 0;
+            var cfg = [];
+            var left = [];
+            var right = [];
+            
+            data.forEach( function(d) {
+                if (!d.length || d.match(/^\s+$/) || d.match(/^\//)) { //empty
+                    return;
+                }
+                if (d.match(/^left:/)) { 
+                    state = 1;
+                    if (left.length ){
+                        
+                        cfg.push({
+                            left : left,
+                            right: right
+                        });
+                        }
+                    left = [];
+                    right = [];
+                    return;
+                }
+                if (d.match(/^right:/)) { 
+                    state = 2;
+                    return;
+                }
+                if (state == 1) {
+                    left.push(d.replace(/\s+/g, ''));
+                    return;
+                }
+                right.push(d.replace(/\s+/g, ''));
+                //Seed.quit();
+               
+            }); 
+            if (left.length ){
+                        
+                cfg.push({
+                    left : left,
+                    right: right
+                });
             }
-            if (d.match(/^left:/)) { 
-                state = 1;
-                if (left.length ){
-                    
-                    cfg.push({
-                        left : left,
-                        right: right
-                    });
-                    }
-                left = [];
-                right = [];
-                return;
-            }
-            if (d.match(/^right:/)) { 
-                state = 2;
-                return;
-            }
-            if (state == 1) {
-                left.push(d.replace(/\s+/g, ''));
-                return;
-            }
-            right.push(d.replace(/\s+/g, ''));
-            //Seed.quit();
-           
-        }); 
-        if (left.length ){
-                    
-            cfg.push({
-                left : left,
-                right: right
-            });
+            this.map = cfg;
+             
         }
-        this.map = cfg;
-         
-         
         
     }
 );
