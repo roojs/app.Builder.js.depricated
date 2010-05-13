254e26f4b4b6376e48506f604662bcc87275f88b
Alan Knowles <alan@akbkhome.com>
2010-05-13 15:14:15 +0800

    Builder/Provider/Palete/Gtk.js

--- Builder/Provider/Palete/Gtk.js
+++ Builder/Provider/Palete/Gtk.js
@@ -81,51 +81,53 @@ Gtk = XObject.define(
         },
         
         
-     commentLoad : function(ns)
-     {
-         
-        if (typeof(this.comments[ns]) != 'undefined') {
-            return;
-        }
-
-        console.log("LOAD DOCS: " + ns);
-        var gi = GIRepository.IRepository.get_default();
-        var ver = gi.get_version(ns);
-        if (!ver) {
-            this.comments[ns] = {};
-            return;
-        }
-        var ret = { };
-
-        // no idea why this is broken on my build system.
-        function getAttribute(n, name){
-            var properties = n.properties;
-            while (properties){
-                if (properties.name == name)
-                     return properties.children.content;
-                 properties = properties.next
-            }
-            return null;
-        }
-                 
-         
-        function walk(element, path) {
+        commentLoad : function(ns)
+        {
              
-             
-            if (!element) {
+            if (typeof(this.comments[ns]) != 'undefined') {
                 return;
             }
-            
-            var n =  getAttribute(element, 'name') ;
-            //console.log("WALK" + n);
-            if (n) {
-                path += path.length ? '.' : '';
-                path += n;
+
+            console.log("LOAD DOCS: " + ns);
+            var gi = GIRepository.IRepository.get_default();
+            var ver = gi.get_version(ns);
+            if (!ver) {
+                this.comments[ns] = {};
+                return;
             }
-            if (element.name == 'return-value') {
-                path += '.return-value';
+            var ret = { };
+
+            // no idea why this is broken on my build system.
+            function getAttribute(n, name){
+                var properties = n.properties;
+                while (properties){
+                    if (properties.name == name)
+                         return properties.children.content;
+                     properties = properties.next
+                }
+                return null;
             }
-             
+                 
+         
+            function walk(element, path) {
+                 
+                 
+                if (!element) {
+                    return;
+                }
+                
+                var n =  getAttribute(element, 'name') ;
+                //console.log("WALK" + n);
+                if (n) {
+                    path += path.length ? '.' : '';
+                    path += n;
+                }
+                if (element.name == 'return-value') {
+                    path += '.return-value';
+                }
+                if (element.name == 'signal') {
+                    path += '.signal';
+                } 
              
                 var d =   getAttribute(element,'doc');
                 if (d) {
