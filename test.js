d5f71aeb4372cc33085763bc3610ca2d7baf94e9
Alan Knowles <alan@akbkhome.com>
2010-05-14 17:18:07 +0800

    test.js

--- test.js
+++ test.js
@@ -5,5 +5,5 @@ XObject = imports.XObject.XObject;
 _top=new XObject({xtype: Gtk.Window, "type":Gtk.WindowType.TOPLEVEL, "id":"builder-0", "xtreepath":"0"})
 ;_top.init();
 _top.el.show_all();
-
-Gtk.main();;
+Gtk.init(null,null);
+Gtk.main();
