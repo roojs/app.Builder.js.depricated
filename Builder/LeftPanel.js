c85ea51e6e3335ea0bf85b8c83318144b44c3164
Alan Knowles <alan@akbkhome.com>
2010-05-14 15:37:00 +0800

    Builder/LeftPanel.js

--- Builder/LeftPanel.js
+++ Builder/LeftPanel.js
@@ -524,7 +524,7 @@ LeftPanel = new XObject({
                                 listeners : {
  
                                     edited : function(r,p, t) {
-                                        print("EDITED?");
+                                        print("EDITED?", p , t);
                                         LeftPanel.get('model').changed(t, true);
                                         LeftPanel.get('model').activePath = false;
                                         //this.el.has_entry = false;
