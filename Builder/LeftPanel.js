5fa904ea7f3b2ea2a2f31adba2674e11ca6d3272
Alan Knowles <alan@akbkhome.com>
2010-05-14 14:23:36 +0800

    Builder/LeftPanel.js

--- Builder/LeftPanel.js
+++ Builder/LeftPanel.js
@@ -494,7 +494,7 @@ LeftPanel = new XObject({
                                     },
                                    
                                     'editing-started' : function(r, e, p) {
-                                        this.el.has_entry = true;
+                                        this.el.has_entry = false;
                                         LeftPanel.get('model').editSelected(e);
                                     }    
                                 },
