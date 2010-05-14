293d5272ea1973b393b45d0171c8172661685470
Alan Knowles <alan@akbkhome.com>
2010-05-14 17:17:44 +0800

    test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+Gtk = imports.gi.Gtk;
+Gdk = imports.gi.Gdk;
+Pango = imports.gi.Pango;
+XObject = imports.XObject.XObject;
+_top=new XObject({xtype: Gtk.Window, "type":Gtk.WindowType.TOPLEVEL, "id":"builder-0", "xtreepath":"0"})
+;_top.init();
+_top.el.show_all();
+
+Gtk.main();;
