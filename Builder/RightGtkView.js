7c6ab56fb402cda1208e40d9c675f6517078f29f
Alan Knowles <alan@akkbhome.com>
2010-05-16 10:15:30 +0800

    Builder/RightGtkView.js

--- Builder/RightGtkView.js
+++ Builder/RightGtkView.js
@@ -71,8 +71,8 @@ RightGtkView = new XObject({
                         xtype : Gtk.Viewport,
                         init : function (parent) {
                             XObject.prototype.init.call(this); 
-                            this.el.set_hadjustment(parent.el.get_hadjustment);
-                            this.el.set_vadjustment(parent.el.get_vadjustment);
+                            this.el.set_hadjustment(parent.el.get_hadjustment());
+                            this.el.set_vadjustment(parent.el.get_vadjustment());
                                 
                         },
                         packing : ['add' ],
