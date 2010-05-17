f305077453038332bfb4b295b2a239d3a9e202af
Alan Knowles <alan@akbkhome.com>
2010-05-17 15:20:03 +0800

    Builder/Provider/Palete/Gtk.js

--- Builder/Provider/Palete/Gtk.js
+++ Builder/Provider/Palete/Gtk.js
@@ -491,6 +491,7 @@ Gtk = XObject.define(
          */
         getDefaultPack: function(pname, cname) {
             var list = this.getPackingList(pname,cname);
+            console.dump(list);
             if (!list.every(function(e) { return e.name =='add'; })) {
                 return 'add'; // add is in our list..?? what about args..?!?
             }
