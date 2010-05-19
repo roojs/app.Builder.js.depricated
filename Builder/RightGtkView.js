c9dbe9e4327be06b7a4b88ae8c68876ef271434d
Alan Knowles <alan@akbkhome.com>
2010-05-19 20:30:42 +0800

    Builder/RightGtkView.js

--- Builder/RightGtkView.js
+++ Builder/RightGtkView.js
@@ -377,6 +377,7 @@ RightGtkView = new XObject({
             });
             
             _top.el.remove(_top.items[0].el);
+             tp.set_size_request(300,300);
              tp.add(_top.items[0].el);
              tp.set_screen();
              tp.show();
