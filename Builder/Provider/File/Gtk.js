501a394f3b8d08e4cc135cacccbbe559be3e934e
Alan Knowles <alan@akbkhome.com>
2010-05-23 21:58:09 +0800

    Builder/Provider/File/Gtk.js

--- Builder/Provider/File/Gtk.js
+++ Builder/Provider/File/Gtk.js
@@ -137,118 +137,41 @@ Gtk = XObject.define(
             
         },
         /** 
-         * fixme - update to new style
+         *  saveJS
+         * 
+         * save as a javascript file.
          * 
          * 
          * 
          */
-        
-        toSourceStdClass: function()
+        saveJS: function()
         {
-            var cfg = this.items[0]
-            var fcfg = XObject.extend({ },  this.items[0]);
-            delete fcfg['*class'];
-            delete fcfg['*extends'];
-            delete fcfg['*static'];
-            delete fcfg['|constructor'];
-            
-            var hasExtends = (typeof(cfg['*extends']) != 'undefined') && cfg['*extends'].length;
-            var hasConstructor = (typeof(cfg['|constructor']) != 'undefined');
-            var isStatic = (typeof(cfg['*static']) == '*static');
-            
-            var newline = '';
-            var endline = '';
-            if (hasExtends) {
-                newline =  hasConstructor ? 
-                
-                 
-                    cfg['//constructor'] + "\n" + 
-                    cfg['*class'] + " = " + cfg['|constructor'] + "\n\n"+ 
-                    "Roo.extend(" + cfg['*class'] + ":, " + cfg['*extends'] + ", " :
-                    
-                    cfg['//*class'] + "\n" + 
-                    cfg['*class'] + " = new " + cfg['*extends'] + "(" ;
-                
-                endline = ');';
-            } else {
-                
-                
-                
-                newline  = hasConstructor ? 
-                
-                    cfg['//constructor'] + "\n" + 
-                    cfg['*class'] + " = " + cfg['|constructor'] + "\n\n"+ 
-                    'Roo.apply( ' +  cfg['*class'] + ".prototype , " :
-                    
-                    cfg['//*class'] + "\n" + 
-                    cfg['*class'] + " = ";
-                
-                    
-                endline = hasConstructor ? ');' : ';';
-            }
-                  
-            return this.outputHeader() + 
-                    newline + 
-                    this.objectToJsString(fcfg,1) +
-                    endline;
-            
-            
-            
-         
-        },
+             
         
-        
-        toSource: function()
-        {
-            // dump the file tree back out to a string.
-            
-            if (typeof(this.items[0]['*class']) != 'undefined') {
-                return this.toSourceStdClass();
-            }
-            
-            
-            // technically there will be different versions of this.
-            var o = this.mungePropObj(this.items[0]);
-            //console.dump(o);
-            //Seed.quit();
-            
-            return this.outputHeader() + 
-                [
-                    "function create()",
-                    "{",
-                    "    return "
-                ].join("\n") +
-                this.objectToJsString(o,2) +
-                [
-                    ";", // end return value..
-                    "}"
-                ].join("\n");
-            
-            
-            
-                
-                    
-                
-                    
-                   
-            
-        },
-       
-        outputHeader : function()
-        {
-            var ret = '//<script type="text/javascript">' + "\n";
-            ret += "\n\n// gi imports\n";
-            Roo.each(this.giImports, function(i) {
-                ret += i + " =  imports.gi." + i + ";\n";
+            var i = [ 'Gtk', 'Gdk', 'Pango', 'GLib', 'Gio', 'GObject', 'GtkSource', 'WebKit', 'Vte' ];
+            var src = "";
+            i.forEach(function(e) {
+                src += e+" = imports.gi." + e +";\n";
             });
-            ret += "\n\n// file imports\n";
-            for(var k in this.imports) {
-                ret += k + " =  imports[" + JSON.stringify(this.imports[k]) + "];\n";
-            }
             
-            return ret + "\n\n";
-        }
-        
+            src += "console = imports.console;\n"; // path?!!?
+            src += "XObject = imports.XObject.XObject;\n"; // path?!!?
+            
+            //if (withDebug) {
+            //    src += "XObject.debug=true;\n"; 
+            //}
+            
+            //this.withDebug = withDebug;
+            src += '_top=new XObject('+ this.mungeToString(data) + ')\n;';
+            src += '_top.init();\n';
+            if (withDebug) {
+                src += "_top.el.show_all();\n"; 
+                src += "Gtk.main();\n"; 
+            }
+            File.write('/tmp/BuilderGtkView.js', src);
+            print("Test code  in /tmp/BuilderGtkView.js");
+            this.lastSrc = src;
+            return src;
         /*
         getTree : function( o ) {
             
