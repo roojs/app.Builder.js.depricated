49e76b94eb66ac466545d276131e223ae85f76ba
Alan Knowles <alan@akbkhome.com>
2010-05-24 17:45:22 +0800

    builder_run.js

--- builder_run.js
+++ builder_run.js
@@ -1,20 +1,3 @@
 //<Script type="text/javascript">
 
-Gtk = imports.gi.Gtk;
-
-
-ProjectManager  = imports.Builder.Provider.ProjectManager.ProjectManager;
-Window          = imports.Builder.Window.Window;
-
-ProjectManager.loadConfig();
-     
-     
-Seed.print('done import window');
-
-Gtk.init(Seed.argv);
-
-Window.init();
- 
-          
-Gtk.main();
- 
+print("Use seed gtkrun.js Builder");
