b7bd624457e4375012efc731c6db19f14d7322bf
Alan Knowles <alan@akbkhome.com>
2010-05-24 14:54:22 +0800

    Sample/builder.js

--- Sample/builder.js
+++ Sample/builder.js
@@ -747,6 +747,8 @@ builder=new XObject({
                                                                                         
                                                                             },
                                                                             dropNode : function(target_data, node) {
+                                                                              print
+                                                                              	  console.dump(target_data);
                                                                               	  console.dump(target_data);
                                                                                         var tp = target_data[0].length ? new  Gtk.TreePath.from_string( target_data[0] ) : false;
                                                                                         
