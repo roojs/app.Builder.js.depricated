219979108633faf43a749fb0c8fee676b00337e9
Alan Knowles <alan@akbkhome.com>
2010-05-24 16:33:07 +0800

    Builder/builder.js

--- Builder/builder.js
+++ Builder/builder.js
@@ -1911,7 +1911,7 @@ builder=new XObject({
                                                                         this.el.set_value(iter, 1, '' +str);
                                                                         this.el.set_value(iter, 3, '' + this.toShort(str));
                                                                         // update the tree...
-                                                                        var LeftTree        = imports.Builder.LeftTree.LeftTree;
+                                                                        //var LeftTree        = imports.Builder.LeftTree.LeftTree;
                                                                         this.get('/LeftTree.model').changed(this.toJS(), doRefresh); 
                                                                     },
                                                                     toJS : function() {
