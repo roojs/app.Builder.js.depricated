88e2f2950274bb310c87b846f4669c340af441c0
Alan Knowles <alan@akbkhome.com>
2010-05-26 20:48:31 +0800

    Sample/Window.js

--- Sample/Window.js
+++ Sample/Window.js
@@ -1083,12 +1083,17 @@ Window=new XObject({
                                                                                 
                                                                                 
                                                                             },
-                                                                            nodeTitle : function(c) {
+                                                                            nodeTitle : function(c, renderfull) {
                                                                                   var txt = [];
                                                                                 c = c || {};
                                                                                 var sr = (typeof(c['+buildershow']) != 'undefined') &&  !c['+buildershow'] ? true : false;
                                                                                 if (sr) txt.push('<s>');
                                                                                 if (typeof(c['*prop']) != 'undefined')   { txt.push(c['*prop']+ ':'); }
+                                                                                
+                                                                                if (renderfull && c['|xns']) {
+                                                                                    txt.push(c['|xns']);
+                                                                                }
+                                                                                
                                                                                 if (c.xtype)      { txt.push(c.xtype); }
                                                                                 if (c.id)      { txt.push('<b>[id=' + c.id + ']</b>'); }
                                                                                 if (c.fieldLabel) { txt.push('[' + c.fieldLabel + ']'); }
@@ -1143,7 +1148,7 @@ Window=new XObject({
                                                                                         }     
                                                                                     },
                                                                             nodeTip : function(c) {
-                                                                                var ret = this.nodeTitle(c);
+                                                                                var ret = this.nodeTitle(c,true);
                                                                                 var funcs = '';
                                                                             
                                                                                 
