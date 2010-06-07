f114016bd6771d0e92a44136be49840e722cf659
Alan Knowles <alan@akbkhome.com>
2010-06-07 16:08:21 +0800

    Sample/DialogTemplateSelect.js

---
+++ Sample/DialogTemplateSelect.js
@@ -0,0 +1,94 @@
+Gtk = imports.gi.Gtk;
+Gdk = imports.gi.Gdk;
+Pango = imports.gi.Pango;
+GLib = imports.gi.GLib;
+Gio = imports.gi.Gio;
+GObject = imports.gi.GObject;
+GtkSource = imports.gi.GtkSource;
+WebKit = imports.gi.WebKit;
+Vte = imports.gi.Vte;
+console = imports.console;
+XObject = imports.XObject.XObject;
+DialogTemplateSelect=new XObject({
+    xtype: Gtk.Dialog,
+    listeners : {
+        delete_event : function (self, event) {
+            this.el.hide();
+            return true;
+        }
+    },
+    default_height : 200,
+    default_width : 400,
+    modal : true,
+    show : function(node) {
+        
+        var pal = this.get('/Window.LeftTree').getPaleteProvider();
+        var opts = pal.listTemplates(node);
+        if (!opts.length) {
+            return node;
+        }
+      
+        opts.unshift('Just add Element')
+        this.get('combo.model').loadData(opts);
+     
+         
+        this.el.show_all();
+        print(this.el.run());
+    },
+    items : [
+        {
+            xtype: Gtk.VBox,
+            pack : function(p,e) {
+                                p.el.get_content_area().add(e.el)
+                            },
+            items : [
+                {
+                    xtype: Gtk.HBox,
+                    pack : "pack_start,false,false,0",
+                    pack : function(p,e) {
+                                        p.el.get_content_area().add(e.el)
+                                    },
+                    items : [
+                        {
+                            xtype: Gtk.Label,
+                            label : "Select Template : ",
+                            pack : "pack_start,false,false"
+                        },
+                        {
+                            xtype: Gtk.ComboBox,
+                            id : "combo",
+                            pack : "add",
+                            init : function() {
+                                XObject.prototype.init.call(this);
+                                 this.el.add_attribute(this.items[0].el , 'markup', 1 );  
+                            },
+                            items : [
+                                {
+                                    xtype: Gtk.CellRendererText,
+                                    pack : false
+                                },
+                                {
+                                    xtype: Gtk.ListStore,
+                                    id : "model",
+                                    pack : "set_model"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            xtype: Gtk.Button,
+            label : "OK",
+            pack : "add_action_widget,1"
+        },
+        {
+            xtype: Gtk.Button,
+            label : "Just Add Element",
+            pack : "add_action_widget,0"
+        }
+    ]
+});
+DialogTemplateSelect.init();
+XObject.cache['/DialogTemplateSelect'] = DialogTemplateSelect;
