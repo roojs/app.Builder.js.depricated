b70a3e7ef0a52a043d9e75402346deab4b530a52
Alan Knowles <alan@akbkhome.com>
2010-06-07 14:58:49 +0800

    Builder/DialogSaveTemplate.js

---
+++ Builder/DialogSaveTemplate.js
@@ -0,0 +1,16 @@
+Gtk = imports.gi.Gtk;
+Gdk = imports.gi.Gdk;
+Pango = imports.gi.Pango;
+GLib = imports.gi.GLib;
+Gio = imports.gi.Gio;
+GObject = imports.gi.GObject;
+GtkSource = imports.gi.GtkSource;
+WebKit = imports.gi.WebKit;
+Vte = imports.gi.Vte;
+console = imports.console;
+XObject = imports.XObject.XObject;
+DialogSaveTemplate=new XObject({
+    xtype: Gtk.Dialog
+});
+DialogSaveTemplate.init();
+XObject.cache['/DialogSaveTemplate'] = DialogSaveTemplate;
