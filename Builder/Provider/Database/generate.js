f90c153f249ebf121886b5165804995e2defa086
Alan Knowles <alan@akbkhome.com>
2010-06-08 13:38:01 +0800

    Builder/Provider/Database/generate.js

--- Builder/Provider/Database/generate.js
+++ Builder/Provider/Database/generate.js
@@ -4,7 +4,18 @@
  * 
  * Let's see if libgda can be used to generate our Readers for roo...
  * 
+ * Concept - conect to database..
+ * 
+ * list tables
+ * 
+ * extra schemas..
+ * 
+ * write readers..
+ * 
+ * 
  */
+ 
+ 
 
 
 
