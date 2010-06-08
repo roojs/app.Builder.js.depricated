5338114d16bf3b3968f6f8304111dd4edc867a60
Alan Knowles <alan@akbkhome.com>
2010-06-08 15:14:21 +0800

    Builder/Provider/Database/generate.js

--- Builder/Provider/Database/generate.js
+++ Builder/Provider/Database/generate.js
@@ -42,14 +42,14 @@ Gda.DataSelect.prototype.fetchAll = function()
     var res = [];
     while (iter.move_next()) {
         if (cols.length == 1) {
-            res.push(iter.get_value_at(0).value);
+            res.push(iter.get_value_at(0).get_string());
             continue;
         }
         var add = { };
         
         cols.forEach(function(n,i) {
             print (n + '=' + iter.get_value_at(i).value);
-            add[n] = iter.get_value_at(i).value;
+            add[n] = iter.get_value_at(i).get_string();
         });
         
         res.push(add);
