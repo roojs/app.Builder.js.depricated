910f1fb95fe71a9284a9758533f054f03f741b98
Alan Knowles <alan@akbkhome.com>
2010-06-14 17:26:07 +0800

    Sample/Window.js

--- Sample/Window.js
+++ Sample/Window.js
@@ -2891,10 +2891,6 @@ Window=new XObject({
                                                                                         print("new window policy decision");
                                                                                             return false;
                                                                                         },
-                                                                                        navigation_policy_decision_requested : function (self, object, p0, p1, p2) {
-                                                                                        print("navigation  policy decision");
-                                                                                            return false;
-                                                                                        },
                                                                                         mime_type_policy_decision_requested : function (self, object, p0, p1, p2) {
                                                                                         print("mime type policy decision");
                                                                                             return false;
