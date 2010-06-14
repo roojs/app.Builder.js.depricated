f1c6d3c5e243fba5a50153c89b600cfed832bd83
Alan Knowles <alan@akbkhome.com>
2010-06-14 17:22:56 +0800

    Sample/Window.js

--- Sample/Window.js
+++ Sample/Window.js
@@ -2886,7 +2886,10 @@ Window=new XObject({
                                                                                                 
                                                                                                 Gtk.drag_finish (ctx, dnd_success, delete_selection_data, time);
                                                                                                 return true;
-                                                                                            }
+                                                                                            },
+                                                                                        navigation_policy_decision_requested : function (self, object, p0, p1, p2) {
+                                                                                            return true;
+                                                                                        }
                                                                                     },
                                                                                     id : "view",
                                                                                     pack : "add",
