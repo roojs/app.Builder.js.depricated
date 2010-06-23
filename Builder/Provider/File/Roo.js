7696b8789461a29d37ec1dd8b00794aae813144c
Alan Knowles <alan@akbkhome.com>
2010-06-23 15:36:15 +0800

    Builder/Provider/File/Roo.js

--- Builder/Provider/File/Roo.js
+++ Builder/Provider/File/Roo.js
@@ -332,45 +332,50 @@ Roo = XObject.define(
             var modkey = this.modOrder + '-' + this.name.replace('/[^A-Z]+/ig', '-');
             
             
-            return [
-                this.outputHeader(),
-                "",
-                "",
-                "// register the module first",
-                "Pman.on('beforeload', function()",
-                "{",
-                "    Pman.register({",
-                "        modKey : '" +modkey+"',",
-                "        module : " + this.name + ",",
-                "        region : '" + this.region   +"',",
-                "        parent : " + (this.parent ||  'false') + ",",
-                "        name : " + JSON.stringify(this.title  || "unnamed module") + ",",
-                "        disabled : " + (this.disabled || 'false') +" ",
-                "    });",
-                "});",
-                "",
+            if (this.name.match(/^Pman/) {
+                    
                 
-                this.name  +  " = new Roo.util.Observable({",
-                "",
-                "    panel : false,",
-                "    disabled : false,",
-                "    parentLayout:  false,",
-                "",
-                "    add : function(parentLayout, region)",
-                "    {",
-                "",
-                "        var _this = this;", // standard avaialbe..
-                "        this.parentLayout = parentLayout;",
-                "",
-                "        this.panel = parentLayout.addxtype(" + o +  ");",
-                "        this.layout = this.panel.layout;",
-                "",
-                "    }",
-                "});",
-                ""
-                 
-                
-             ].join("\n");
+                // old BC way we did things..
+                return [
+                    this.outputHeader(),
+                    "",
+                    "",
+                    "// register the module first",
+                    "Pman.on('beforeload', function()",
+                    "{",
+                    "    Pman.register({",
+                    "        modKey : '" +modkey+"',",
+                    "        module : " + this.name + ",",
+                    "        region : '" + this.region   +"',",
+                    "        parent : " + (this.parent ||  'false') + ",",
+                    "        name : " + JSON.stringify(this.title  || "unnamed module") + ",",
+                    "        disabled : " + (this.disabled || 'false') +" ",
+                    "    });",
+                    "});",
+                    "",
+                    
+                    this.name  +  " = new Roo.util.Observable({",
+                    "",
+                    "    panel : false,",
+                    "    disabled : false,",
+                    "    parentLayout:  false,",
+                    "",
+                    "    add : function(parentLayout, region)",
+                    "    {",
+                    "",
+                    "        var _this = this;", // standard avaialbe..
+                    "        this.parentLayout = parentLayout;",
+                    "",
+                    "        this.panel = parentLayout.addxtype(" + o +  ");",
+                    "        this.layout = this.panel.layout;",
+                    "",
+                    "    }",
+                    "});",
+                    ""
+                     
+                    
+                 ].join("\n");
+            }
         },
             
         guessName : function(ar) // turns the object into full name.
