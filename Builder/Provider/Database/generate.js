88c7fae8ef98ec76768f23cb5fe4b4cf9a22818e
Alan Knowles <alan@akbkhome.com>
2010-06-23 10:38:52 +0800

    Builder/Provider/Database/generate.js

--- Builder/Provider/Database/generate.js
+++ Builder/Provider/Database/generate.js
@@ -25,7 +25,7 @@ Gda.init();
 
 var prov = Gda.Config.list_providers ();
 //print(prov.dump_as_string());
-print(JSON.stringify(Seed.args));
+print(JSON.stringify(Seed.argv));
 Seed.quit();
 var db_name = "pman";
 var   cnc = Gda.Connection.open_from_string ("MySQL", "DB_NAME=" + db_name, 
