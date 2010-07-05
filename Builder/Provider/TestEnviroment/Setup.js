8ce3d0be4b9dce05fe32a10df9fe812d442c6cf3
Alan Knowles <alan@akbkhome.com>
2010-07-05 18:07:18 +0800

    Builder/Provider/TestEnviroment/Setup.js

--- Builder/Provider/TestEnviroment/Setup.js
+++ Builder/Provider/TestEnviroment/Setup.js
@@ -18,13 +18,37 @@ Setup = {
     
     dir:  '', // home appbulider...
     
+    
     cloneModules : function()
     {
-        if (File.isDirectory(this.dir + '/Pman.Base')) {
-            Spawn.run(this.dir + '/Pman.Base', 'git pull');
-        }
-        Spawn.run(this.dir, 'git clone http://git.akbkhome.com/Pman.Base');
+        this.cloneModule('Pman.Core');
+        this.cloneModule('Pman.Base');
+        this.cloneModule('pear');
+        this.cloneModule('roojs1');
+        this.link({
+            'Pman.Base/Pman.php'        : 'Pman.php',
+            'Pman.Base/Pman/Roo.php'    : 'Pman/Roo.php',
+            'Pman.Base/Pman/Login.php'  : 'Pman/Login.php',
+            'Pman.Base/Pman/Images.php' : 'Pman/Images.php',
+            'Pman.Base/Pman/I18N.php'   : 'Pman/I18N.php',
+            'Pman.Base/Pman/GnumericToExcel.php' : 'Pman/GnumericToExcel.php',
+            'Pman.Base/Pman/templates' : 'Pman/templates',
+            'Pman.Core' : 'Pman/Core',
+            
+            
+            
+            
     },
+    cloneModule : function(name)
+    {
+        if (File.isDirectory(this.dir + '/' + name)) {
+            Spawn.run(this.dir + '/' + name, 'git pull');
+            return;
+        }
+        Spawn.run(this.dir, 'git clone http://git.akbkhome.com/' + name);
+    },
+    
+    
     
     
     
