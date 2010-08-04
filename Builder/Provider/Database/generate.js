11a5f644a8edd64c9d4db3e2f721b1b1f1c701cf
Alan Knowles <alan@akbkhome.com>
2010-08-04 23:46:17 +0800

    Builder/Provider/Database/generate.js

--- Builder/Provider/Database/generate.js
+++ Builder/Provider/Database/generate.js
@@ -73,10 +73,10 @@ Gda.DataSelect.prototype.fetchAll = function()
         var add = { };
         
         cols.forEach(function(n,i) {
-           var val = _this.get_value_at(i,r);
-           var type = GObject.type_name(val.g_type) ;
-           var vs = type == 'GdaBlob' ? val.value.to_string(1024) : val.value;
-           print(n + " : TYPE: " + GObject.type_name(val.g_type) + " : " + vs);
+           v ar val = _this.get_value_at(i,r);
+            var type = GObject.type_name(val.g_type) ;
+            var vs = type == 'GdaBlob' ? val.value.to_string(1024) : val.value;
+            print(n + " : TYPE: " + GObject.type_name(val.g_type) + " : " + vs);
             print (n + '=' + iter.get_value_at(i).value);
             add[n] = vs;
         });
