969b1399a2524257908433b3cc7eb309f5013ea3
Alan Knowles <alan@akbkhome.com>
2010-08-04 23:47:22 +0800

    Builder/Provider/Database/generate.js

--- Builder/Provider/Database/generate.js
+++ Builder/Provider/Database/generate.js
@@ -57,7 +57,7 @@ Gda.DataSelect.prototype.fetchAll = function()
     for (var i =0;i < this.get_n_columns(); i++) {
         cols.push(this.get_column_name(i));
     }
-    print(JSON.stringify(cols, null,4));
+    //print(JSON.stringify(cols, null,4));
     var iter = this.create_iter();
     var res = [];
     //print(this.get_n_rows());
