fe33f8f4c070f0046e6b3dc3162204484fadf0b9
Alan Knowles <alan@akbkhome.com>
2010-10-21 17:02:11 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
  
 #// compile GIR's
-mkdir ~/.Builder/girepository-1.1/ || false
+mkdir -p ~/.Builder/gir-1.1/girepository-1.0 || false
  
 ls gir-1.1 | sed s/.gir// | awk \
-    '{ print "g-ir-compiler  gir-1.1/" $1 ".gir -o  ~/.Builder/gir-1.1/girepositor-1.0" $1 ".typelib" }' \
+    '{ print "g-ir-compiler  gir-1.1/" $1 ".gir -o  ~/.Builder/gir-1.1/girepository-1.0" $1 ".typelib" }' \
     | sh -x
