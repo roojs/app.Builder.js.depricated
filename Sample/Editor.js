b7bccdb72d306274f62a6ce788ec363b57f6332f
Alan Knowles <alan@akbkhome.com>
2010-10-29 17:51:08 +0800

    Sample/Editor.js

--- Sample/Editor.js
+++ Sample/Editor.js
@@ -65,8 +65,11 @@ Editor=new XObject({
                             xtype: GtkSource.View,
                             listeners : {
                                 key_release_event : function (self, event) {
+                                    if (event.key.keyval != 115 || !(Gdk.ModifierType.CONTROL_MASK & 4) ) {
+                                        return;
+                                    }
                                     print(event.key.keyval)
-                                    //Gdk.Control_L
+                                    //Gdk.ModifierType.CONTROL_MASK
                                     return false;
                                 }
                             },
