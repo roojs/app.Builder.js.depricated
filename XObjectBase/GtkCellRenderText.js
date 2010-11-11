bb0832b0fec07b35038445b770440e8aead29627
Alan Knowles <alan@akkbhome.com>
2010-11-12 07:08:29 +0800

    fix deleting hopefully

---
+++ XObjectBase/GtkCellRenderText.js
@@ -0,0 +1,10 @@
+
+//<Script type="Text/javascript">
+
+XObject = imports.XObject.XObject
+ 
+// Cell render text..
+
+GtkCellRenderText = {
+    pack : 'pack_start'
+}; 
---
+++ XObjectBase/GtkTreeViewColumn.js
@@ -0,0 +1,13 @@
+
+
+//<Script type="Text/javascript">
+
+XObject = imports.XObject.XObject
+ 
+// Cell render text..
+
+
+GtkTreeViewColumn = {
+    pack : 'append_column'
+}; 
+
