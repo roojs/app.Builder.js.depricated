33b77ccf40fd45534f44565a29257120c6f77471
Alan Knowles <alan@akbkhome.com>
2010-11-23 18:52:29 +0800

    XObject.js

--- XObject.js
+++ XObject.js
@@ -568,7 +568,7 @@ XObject.extend(XObject,
      * @property {Boolean} debug XObject  debugging.  - set to true to debug.
      * 
      */
-    debug : false,
+    debug : true,
     /**
      * @property {Object} cache - cache of object ids
      * 
