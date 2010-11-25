0352c7dde260ab9537b2792dee434579071714a1
Alan Knowles <alan@akbkhome.com>
2010-11-25 15:36:29 +0800

    XObjectBase/GtkTreeStore.js

---
+++ XObjectBase/GtkTreeStore.js
@@ -0,0 +1,15 @@
+
+//<Script type="Text/javascript">
+
+XObject = imports.XObject.XObject
+ 
+//GtkClutter.Embed..
+// children are not added at init / but at show stage..
+// listener is added on show..
+// we should really add a hock to destroy it..
+
+GtkTreeStore= {
+    
+    pack : 'set_model'
+    
+}; 
