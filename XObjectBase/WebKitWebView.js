3cfcc94a4472d8b5d83f955fccd4d7213dba4026
Alan Knowles <alan@akbkhome.com>
2010-11-26 15:43:18 +0800

    XObjectBase/WebKitWebView.js

--- XObjectBase/WebKitWebView.js
+++ XObjectBase/WebKitWebView.js
@@ -6,18 +6,26 @@ XObject = imports.XObject.XObject
 // children are not added at init / but at show stage..
 // listener is added on show..
 // we should really add a hock to destroy it..
-WebKitWebView  = {
- 
-    pack : function(parent, item)
-    {
-        
-        if (XObject.type(parent.xtype) == 'GtkScrolledWindow') {
-            parent.el.add(this.el);
-            return;
-        }
-        XObject.fatal("do not know how to pack webview to" +  XObject.type(parent.xtype));
-        
-    } 
-     
 
-};
+
+WebKitWebView = XObject.define(
+    function(cfg) {
+        XObject.call(this, cfg);
+     
+    }, 
+    XObject,
+    {
+        pack : function(parent, item)
+        {
+            
+            if (XObject.type(parent.xtype) == 'GtkScrolledWindow') {
+                parent.el.add(this.el);
+                return;
+            }
+            XObject.fatal("do not know how to pack webview to" +  XObject.type(parent.xtype));
+            
+        } 
+    }
+ 
+); 
+  
