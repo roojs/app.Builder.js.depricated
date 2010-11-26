597ce823f10349f0f874200d86b9618ea123b97b
Alan Knowles <alan@akbkhome.com>
2010-11-26 15:44:56 +0800

    XObjectBase/GtkTreeStore.js

--- XObjectBase/GtkTreeStore.js
+++ XObjectBase/GtkTreeStore.js
@@ -7,7 +7,17 @@ XObject = imports.XObject.XObject
 // children are not added at init / but at show stage..
 // listener is added on show..
 // we should really add a hock to destroy it..
+GtkTreeStore = XObject.define(
+    function(cfg) {
+        XObject.call(this, cfg);
 
+    }, 
+    XObject,
+    {
+         pack : 'set_model'
+    }
+ 
+); 
 GtkTreeStore= {
     
     pack : 'set_model'
