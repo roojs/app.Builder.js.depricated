bdc1d8f634d9e3473a7a12800993f50b44a6dfd5
Alan Knowles <alan@akkbhome.com>
2010-11-30 21:23:24 +0800

    XObjectBase/GdlDock.js

---
+++ XObjectBase/GdlDock.js
@@ -0,0 +1,16 @@
+
+//<Script type="Text/javascript">
+
+XObject = imports.XObject.XObject
+ 
+// Cell render text..
+
+GdlDock = XObject.define(
+    function(cfg) {
+        XObject.call(this, cfg);
+    }, 
+    XObject,
+    {
+        pack : 'pack_start'
+    }
+}; 
