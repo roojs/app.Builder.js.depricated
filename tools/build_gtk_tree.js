6ba995e48fb99b662ed174dfb5f3cf5e73521e94
Alan Knowles <alan@akbkhome.com>
2010-12-01 16:14:16 +0800

    tools/build_gtk_tree.js

--- tools/build_gtk_tree.js
+++ tools/build_gtk_tree.js
@@ -24,8 +24,7 @@ var ns_list = NameSpace.namespaces();
 ns_list = ns_list.sort();
 // let's try and load them, so we find out early what will fail.
 print("loading library to make sure it works.");
-ns_list.forEach(function(ns_name) 
-{   
+ns_list.forEach(function(ns_name) {   
     var  core = imports.gi[ns_name];
 });
 
