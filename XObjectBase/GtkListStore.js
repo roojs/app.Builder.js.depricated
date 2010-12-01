c8398e727caa6601fb53900b5c6df0c634652a48
Alan Knowles <alan@akkbhome.com>
2010-12-01 21:17:23 +0800

    XObjectBase/GtkListStore.js

--- XObjectBase/GtkListStore.js
+++ XObjectBase/GtkListStore.js
@@ -10,7 +10,16 @@ XObject = imports.XObject.XObject
 GtkListStore = XObject.define(
     function(cfg) {
         XObject.call(this, cfg);
-
+        // this is an example...
+        this.el.set_column_types ( 6, [
+            GObject.TYPE_STRING,  // real key
+            GObject.TYPE_STRING, // real type
+            GObject.TYPE_STRING, // docs ?
+            GObject.TYPE_STRING, // visable desc
+            GObject.TYPE_STRING, // function desc
+            GObject.TYPE_STRING // element type (event|prop)
+            
+        ] );
     }, 
     XObject,
     {
