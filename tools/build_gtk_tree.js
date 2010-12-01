50c3709479770dff0b09dc933e3a60a9000e4092
Alan Knowles <alan@akbkhome.com>
2010-12-01 16:02:28 +0800

    tools/build_gtk_tree.js

---
+++ tools/build_gtk_tree.js
@@ -0,0 +1,9 @@
+//<script type="text/javascript">
+
+// see if we can build the insertion tree for gtk - using introspection
+
+// it should build the tree of feasible insertions, then we will have to manually prune it..
+
+// it needs to know
+// a) what the inherited types are
+// b) what methods are available for each type, that include a reference to another type..
