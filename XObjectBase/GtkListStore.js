ba5d7bde66174d26d9a742c0195c74b9408f362e
Alan Knowles <alan@akkbhome.com>
2010-12-01 21:16:41 +0800

    XObjectBase/GtkListStore.js

---
+++ XObjectBase/GtkListStore.js
@@ -0,0 +1,20 @@
+
+//<Script type="Text/javascript">
+
+XObject = imports.XObject.XObject
+ 
+//GtkClutter.Embed..
+// children are not added at init / but at show stage..
+// listener is added on show..
+// we should really add a hock to destroy it..
+GtkListStore = XObject.define(
+    function(cfg) {
+        XObject.call(this, cfg);
+
+    }, 
+    XObject,
+    {
+        pack : 'set_model'
+    }
+ 
+); 
