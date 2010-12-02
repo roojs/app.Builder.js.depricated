d988d3b22deec23eaebaab69086359225bf0b314
Alan Knowles <alan@akbkhome.com>
2010-12-02 17:05:15 +0800

    tools/build_gtk_tree.js

--- tools/build_gtk_tree.js
+++ tools/build_gtk_tree.js
@@ -25,66 +25,76 @@ File        = imports.File.File;
 NameSpace   = imports.Introspect.NameSpace.NameSpace; 
 Link        = imports.Introspect.Link.Link; 
 
-var ns_list = [ 'Gtk' ] ; //NameSpace.namespaces();
+
+
+function BuildLists () {
  
-ns_list = ns_list.sort();
-// let's try and load them, so we find out early what will fail.
-print("loading library to make sure it works.");
 
-var classes = {};
+    var ns_list = [ 'Gtk' ] ; //NameSpace.namespaces();
+     
+    ns_list = ns_list.sort();
+    // let's try and load them, so we find out early what will fail.
+    print("loading library to make sure it works.");
 
-ns_list.forEach(function(ns_name) {   
-    var  core = imports.gi[ns_name];
-    var ns = NameSpace.ns(ns_name); // fetch all the elements in namespace...
-    ns['objects'].forEach( function(n) {
-        var odata = NameSpace.factory('Class', ns_name, n);
-        classes[odata.alias] = odata;
-        
-    });
-    ns['interfaces'].forEach( function(n) {
-         var odata =NameSpace.factory('Interface', ns_name, n);
-        classes[odata.alias] = odata;
-    });
-});
+    var classes = {};
 
-
-print("Looping throught namespaces");
-var ns_idx = [];
-var implementations = {};
-var methods = {}
-   
-for (cls in classes) {
-    var odata = classes[cls];
-    methods[cls] = {}
-       
-    implementations[odata.alias] = odata.titleType == 'Class' ? odata.childClasses :  odata.implementedBy;  
-    //print(JSON.stringify(odata.methods,null,4));
-    odata.methods.forEach(function(m) {
-       
-        m.params.forEach(function(p) {
-             
-            if (!p.type || typeof(classes[p.type]) == 'undefined') {
-                return;
-            }
-            if (typeof(methods[cls][p.type]) == 'undefined') {
-                methods[cls][p.type] = [];
-            }
-            if (methods[cls][p.type].indexOf(m.name) > -1) {
-                return;
-            }
-            methods[cls][p.type].push(m.name);
+    ns_list.forEach(function(ns_name) {   
+        var  core = imports.gi[ns_name];
+        var ns = NameSpace.ns(ns_name); // fetch all the elements in namespace...
+        ns['objects'].forEach( function(n) {
+            var odata = NameSpace.factory('Class', ns_name, n);
+            classes[odata.alias] = odata;
             
         });
-        
+        ns['interfaces'].forEach( function(n) {
+             var odata =NameSpace.factory('Interface', ns_name, n);
+            classes[odata.alias] = odata;
+        });
     });
-    //for(method in odata.methods) {
-    //    print(method.name);
-    //}
-    
+
+
+    print("Looping throught namespaces");
+    var ns_idx = [];
+    var implementations = {};
+    var methods = {}
+       
+    for (cls in classes) {
+        var odata = classes[cls];
+        methods[cls] = {}
+           
+        implementations[odata.alias] = odata.titleType == 'Class' ? odata.childClasses :  odata.implementedBy;  
+        //print(JSON.stringify(odata.methods,null,4));
+        odata.methods.forEach(function(m) {
+           
+            m.params.forEach(function(p) {
+                 
+                if (!p.type || typeof(classes[p.type]) == 'undefined') {
+                    return;
+                }
+                if (typeof(methods[cls][p.type]) == 'undefined') {
+                    methods[cls][p.type] = [];
+                }
+                if (methods[cls][p.type].indexOf(m.name) > -1) {
+                    return;
+                }
+                methods[cls][p.type].push(m.name);
+                
+            });
+            
+        });
+        //for(method in odata.methods) {
+        //    print(method.name);
+        //}
+        
+        
+    }
+    this.methods = methods;
+    this.implementations = implementations;
+    //print(JSON.stringify(methods,null,4));
+    //print(JSON.stringify(implementations,null,4));
     
 }
-print(JSON.stringify(methods,null,4));
-print(JSON.stringify(implementations,null,4));
+
 
 // we now have a list of classes / methods that can be used..
 // we now need a ui to flag stuff as "don't bother with"
