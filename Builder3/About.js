cde9c36620fd3171cfa745c1d6645ae2c36bc5cd
Alan <alan@wideboyHD>
2011-07-21 00:09:45 +0800

    Builder3/About.js

--- Builder3/About.js
+++ Builder3/About.js
@@ -7,7 +7,7 @@ GObject = imports.gi.GObject;
 GtkSource = imports.gi.GtkSource;
 WebKit = imports.gi.WebKit;
 Vte = imports.gi.Vte;
- console = imports.console;
+console = imports.console;
 XObject = imports.XObject.XObject;
 About=new XObject({
     xtype: Gtk.AboutDialog,
