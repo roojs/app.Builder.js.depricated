839e5d1b5104968880b6ac1f4617c3192a38d930
Alan Knowles <alan@akbkhome.com>
2011-08-23 13:33:20 +0800

    rconv.js

--- rconv.js
+++ rconv.js
@@ -59,7 +59,7 @@ print("------------ out ------------------");
 // now try and render it back to javascript.
 var rclass = imports.JsRender[rf.cfg.type][rf.cfg.type];
 var render = new rclass(rf.cfg);
-
+print(render.toSource());
 
 print(JSON.stringify(rf.cfg, null,4));
  
