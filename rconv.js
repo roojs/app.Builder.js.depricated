1c3abfd4bd1116126042362fbe91364dcd650317
Alan Knowles <alan@akbkhome.com>
2011-08-23 13:16:26 +0800

    rconv.js

---
+++ rconv.js
@@ -0,0 +1,63 @@
+/**
+ *
+ * This is a test file to convert appbuilder style application code
+ *  into javascript structures.
+ *
+ *
+ * Needs to test a while directory convert to-><-from and compare..
+ *
+ *
+ */
+
+var RooFile = imports.JSDOC.RooFile.RooFile;
+var TokenReader = imports.JSDOC.TokenReader.TokenReader;
+var TextStream = imports.JSDOC.TextStream.TextStream;
+
+var File = imports.File.File;
+
+
+
+
+
+var args = Array.prototype.slice.call(Seed.argv);
+
+args.shift();
+args.shift();
+//print(JSON.stringify(args, null,4));
+//Seed.quit();
+
+
+
+var tr = new  TokenReader(  { 
+    keepDocs :true, 
+    keepWhite : true,  
+    keepComments : true, 
+    sepIdents : false,
+    collapseWhite : true,
+    filename : args[0],
+    ignoreBadGrammer: true
+});
+
+var str = File.read(args[0])
+
+var toks = tr.tokenize(new TextStream(str));  
+
+
+var rf = new RooFile(toks);
+//print(JSON.stringify(rf.tokens, null,4));Seed.quit();
+
+
+
+
+rf.parse();
+
+print("------------ in ------------------");
+print(str);
+print("------------ out ------------------");
+
+print(JSON.stringify(rf.cfg, null,4));
+
+print("------------ was ------------------");
+
+print(File.read(args[0].replace(/\.js$/,'.bjs')));
+
