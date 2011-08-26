df30e3934b09aa49678a87a56cb0c0a5a30cebbc
Alan Knowles <alan@akbkhome.com>
2011-08-26 13:50:25 +0800

    JsRender/Base.js

--- JsRender/Base.js
+++ JsRender/Base.js
@@ -254,9 +254,7 @@ Base = XObject.define(
                 
             }
             
-            if (isArray) {
-                print(JSON.stringify(keys, null,4));
-            }
+            //if (isArray) { print(JSON.stringify(keys, null,4)); }
             // keys is just the real keys of the object.
             var _this = this;
             
