d04e7c6316f31667ccec4a1a762bc4f657cc3a35
Alan Knowles <alan@akbkhome.com>
2011-09-09 13:46:48 +0800

    Palete/Gtk.js

--- Palete/Gtk.js
+++ Palete/Gtk.js
@@ -201,7 +201,7 @@ Gtk = XObject.define(
                 print("COULND NOT FIND BY NAME");
                 return [];
             }
-            var etype = bi.get_type();;
+            var etype = GIRepository.base_info_get_type(.bi);
             var meth = etype == GIRepository.InfoType.INTERFACE ?
                 [ 
                     'get_n_properties',
