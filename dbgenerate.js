f88564538730fd3f83ffc1683ec596a6b5348aec
Alan Knowles <alan@akbkhome.com>
2012-02-27 12:36:40 +0800

    dbgenerate.js

--- dbgenerate.js
+++ dbgenerate.js
@@ -236,7 +236,9 @@ switch(opts.DBTYPE) {
         query_tables = "select c.relname FROM pg_catalog.pg_class c " + 
             "LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace " + 
             "WHERE c.relkind IN ('r','') AND n.nspname NOT IN ('pg_catalog', 'pg_toast')" +
-            "AND pg_catalog.pg_table_is_visible(c.oid) " + 
+            "AND pg_catalog.pg_table_is_visible(c.oid) ";
+            
+        break;
 
     
     
