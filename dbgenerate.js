fd4a4d48d60f4815e6ebb3c761f1babcb3946964
Alan Knowles <alan@akbkhome.com>
2012-02-27 13:06:47 +0800

    dbgenerate.js

--- dbgenerate.js
+++ dbgenerate.js
@@ -301,6 +301,7 @@ tables.forEach(function(table) {
     
     schema.forEach(function(e)  {
         e.Type = e.type;
+        e.field = e.field;
         if (!e.Type) {
             print(JSON.stringify(e, null,4));    Seed.quit();
         }
