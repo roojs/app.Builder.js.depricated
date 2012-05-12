c25338fcb13900051b0ea1ac3e3212cde21d7eac
Alan Knowles <alan@roojs.com>
2012-05-12 22:28:42 +0800

    builder.sh

--- builder.sh
+++ builder.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # use our overridden typelibs.
-export GI_TYPELIB_PATH=~/.Builder/girepository-1.1
-
+export GI_TYPELIB_PATH=~/.Builder/girepository-1.2
  
 cd $(dirname $0)
 
 # run theapplication.
-seed ./gtkrun.js Sample
+seed ./gtkrun.js Builder3 
+
