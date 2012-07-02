144fd3114eec31d2719f91b3f1d478315fd56ee3
Alan Knowles <alan@akbkhome.com>
2012-07-02 21:39:03 +0800

    Builder3/Main.js

--- Builder3/Main.js
+++ Builder3/Main.js
@@ -1,5 +1,23 @@
+Gtk = imports.gi.Gtk;
+Gdk = imports.gi.Gdk;
+
 Window = imports.Builder3.Window.Window;
- 
+
+
+atoms = {
+               "STRING" : Gdk.atom_intern("STRING")
+    	};
+targetList = new Gtk.TargetList();
+targetList.add(  atoms["STRING"], 0, 0);
+
+
+
+Gtk.rc_parse_string(
+            "style \"gtkcombobox-style\" {\n" + 
+            "    GtkComboBox::appears-as-list = 1\n" +
+            "}\n"+
+            "class \"GtkComboBox\" style \"gtkcombobox-style\"\n");
+
 
 Window.el.show_all();
  
