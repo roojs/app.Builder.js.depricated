9a8e1da598e888f1c0f72443c57662185b37f062
Alan Knowles <alan@akbkhome.com>
2012-07-03 23:09:50 +0800

    XObjectBase/GtkExpander.js

--- XObjectBase/GtkExpander.js
+++ XObjectBase/GtkExpander.js
@@ -1,12 +1,6 @@
 
-//<Script type="Text/javascript">
-
 XObject = imports.XObject.XObject
  
-//GtkClutter.Embed..
-// children are not added at init / but at show stage..
-// listener is added on show..
-// we should really add a hock to destroy it..
 
 GtkExpander = XObject.define(
     function(cfg) {
