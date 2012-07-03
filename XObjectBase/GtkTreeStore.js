ca1b02a52f2bd71570e50d1d54a9352d28d64369
Alan Knowles <alan@akbkhome.com>
2012-07-03 23:24:17 +0800

    XObjectBase/GtkTreeStore.js

--- XObjectBase/GtkTreeStore.js
+++ XObjectBase/GtkTreeStore.js
@@ -19,14 +19,23 @@ GtkTreeStore = XObject.define(
         init : function() 
         {
             XObject.prototype.init.call(this);
-            this.el.set_column_types ( 6, [
-                GObject.TYPE_STRING, 
-                GObject.TYPE_STRING, 
-                GObject.TYPE_STRING, 
-                GObject.TYPE_STRING, 
-                GObject.TYPE_STRING, 
-                GObject.TYPE_STRING 
-            ] );
+            
+            if (!this.columns) { 
+                
+                this.el.set_column_types ( 6, [
+                    GObject.TYPE_STRING, 
+                    GObject.TYPE_STRING, 
+                    GObject.TYPE_STRING, 
+                    GObject.TYPE_STRING, 
+                    GObject.TYPE_STRING, 
+                    GObject.TYPE_STRING 
+                ] );
+            } else {
+                 this.el.set_column_types ( this.columns.length, this.columns);
+                  
+                
+            }
+            
             
         }
     }
