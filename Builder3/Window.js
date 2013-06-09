dd23b45ec11554a1d7e1889bb7829bac68d68402
Alan Knowles <alan@akbkhome.com>
2013-06-09 20:25:03 +0800

    Builder3/Window.js

--- Builder3/Window.js
+++ Builder3/Window.js
@@ -1565,7 +1565,7 @@ Window=new XObject({
                                                                                     
                                                                                     loadData : function(data) {
 																						//print("load data called");Seed.quit();
-																						print(data);Seed.quit();
+																						//print(data);Seed.quit();
 																						
                                                                                         var ov = this.get('/LeftProjectTree.combo').getValue();
                                                                                         this.el.clear();
