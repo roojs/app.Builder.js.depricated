3e44a2259222a0b28e918bb03ba9544a6e059ce0
Alan Knowles <alan@akbkhome.com>
2013-06-09 20:37:38 +0800

    ProjectManager.js

--- ProjectManager.js
+++ ProjectManager.js
@@ -74,7 +74,7 @@ ProjectManager =  new Observable({
             
              
         }
-        print(typeof(this.projects));Seed.quit();
+        print(JSON.stringify(this.projects));Seed.quit();
    
         this.projects.sort(function(a,b) {
             if (a.getName() == b.getName()) {
