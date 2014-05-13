ff178f6a1e1057738a742f408d08514bf0e023a0
Alan Knowles <alan@akbkhome.com>
2014-05-13 22:45:24 +0800

    Project/Base.js

--- Project/Base.js
+++ Project/Base.js
@@ -300,7 +300,7 @@ Base = XObject.define(
                     subs.push(dir  + '/' + fn);
                     return;
                 }
-                print(fn);
+                //print(fn);
                 if (!fn.match(/\.bjs$/)) {
                     return;
                 }
@@ -318,7 +318,7 @@ Base = XObject.define(
                 }
                 var xt = _this.xtype;
                 var cls = imports.JsRender[xt][xt];
-                
+                print(fn);
                 //Seed.print("Adding file " + dir  + '/' + fn);
                 _this.files[dir  + '/' + fn] = new cls({
                     path : dir  + '/' + fn,
