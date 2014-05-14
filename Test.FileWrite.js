e7fec3e205dffd7b02cc1d502ce6bb6336a0be50
Alan Knowles <alan@roojs.com>
2014-05-14 20:35:29 +0800

    Test.FileWrite.js

--- Test.FileWrite.js
+++ Test.FileWrite.js
@@ -3,9 +3,9 @@
 var pr = new imports.Project.Gtk.Gtk({
     xtype : "Gtk",
     name : "Builder4",
-    paths : { "/home/alan/gitlive/app.Builder/Builder4" : "dir" }
+    paths : { "/home/alan/gitlive/app.Builder.js/Builder4" : "dir" }
     
 });
 
-var f = pr.files['/home/alan/gitlive/app.Builder/Builder4'];
+var f = pr.files['/home/alan/gitlive/app.Builder.js/Builder4'];
 print(JSON.stringify(f));
