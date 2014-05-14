480aef6d77f1d8c8bee1a0b9bcbec229e3e51eb0
Alan Knowles <alan@roojs.com>
2014-05-14 20:37:14 +0800

    Test.FileWrite.js

--- Test.FileWrite.js
+++ Test.FileWrite.js
@@ -7,7 +7,7 @@ var pr = new imports.Project.Gtk.Gtk({
     
 });
 print(JSON.stringify(pr.files,null,4));
-var f = pr.files['/home/alan/gitlive/app.Builder.js/Builder4/About.bjs'];
+var f = pr.files['/home/alan/gitlive/app.Builder.js/Builder4/Editor.bjs'];
 print(JSON.stringify(f,null,4));
 
 
