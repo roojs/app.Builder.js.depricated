407c86367f66a5130f922db02dabc51ed0c91cf0
Alan Knowles <alan@akbkhome.com>
2014-05-18 23:16:43 +0800

    JsRender/Gtk.js

--- JsRender/Gtk.js
+++ JsRender/Gtk.js
@@ -295,8 +295,8 @@ Gtk = XObject.define(
             
             // public static?
             if (!depth) {
-                strbuilder(ipad + " _this = this;\n");
-                strbuilder(ipad + " " + item.id  + " = this;\n");
+                strbuilder(ipad + "_this = this;\n");
+                strbuilder(ipad +  item.id  + " = this;\n");
             } else {
                 if (item.xvala_id !== false) {
                     strbuilder(ipad + " _this." + item.xvala_id  + " = this;\n");
