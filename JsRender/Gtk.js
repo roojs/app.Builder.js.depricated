791749616474eb08d4406ed501cb1bbf1408f43b
Alan Knowles <alan@akbkhome.com>
2014-05-19 23:57:59 +0800

    JsRender/Gtk.js

--- JsRender/Gtk.js
+++ JsRender/Gtk.js
@@ -398,7 +398,7 @@ Gtk = XObject.define(
                          //print(JSON.stringify(vv));Seed.quit();
                          vv = vv.replace(/^\n+/,'');
                          vv = vv.replace(/\n+$/,'');
-                         vv = vv.replace(/\n/g,"\n" + ipad);
+                         vv = vv.replace(/\n/g,"\n" + pad);
                          strbuilder(pad + vv  + "\n");
                     }
             }
