4ed0090e228be3f92ae62d3c6f673fafb2db11dc
Alan Knowles <alan@roojs.com>
2014-06-10 19:48:28 +0800

    Test.GtkWriter.js

--- Test.GtkWriter.js
+++ Test.GtkWriter.js
@@ -11,14 +11,14 @@
 
 var proj = new imports.Project.Gtk.Gtk({
     name : "Test",
-    paths : { "/home/alan/gitlive/app.Builder/Builder4" : "dir" },
+    paths : { "/home/alan/gitlive/app.Builder.js/Builder4" : "dir" },
     xtype :  "Gtk",
     
 });
 
 
 
-var tf = proj.files['/home/alan/gitlive/app.Builder/Builder4/DialogNewComponent.bjs'];
+var tf = proj.files['/home/alan/gitlive/app.Builder.js/Builder4/DialogNewComponent.bjs'];
 tf.loadItems(function() {
     
     
