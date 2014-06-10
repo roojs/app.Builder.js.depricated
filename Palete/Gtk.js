2ff41583e91980c545aa9ad58a4f97384d80c6df
Alan Knowles <alan@roojs.com>
2014-06-10 20:02:47 +0800

    Palete/Gtk.js

--- Palete/Gtk.js
+++ Palete/Gtk.js
@@ -416,6 +416,7 @@ Gtk = XObject.define(
         typeToName  : function (type_info, allow_iface) // find type for properties or arguments.
         {
             var ty = GIRepository.type_tag_to_string( GIRepository.type_info_get_tag(type_info));
+            print(ty);
            
             if ((ty == 'void') && GIRepository.type_info_is_pointer(type_info)) {
                 return false;
