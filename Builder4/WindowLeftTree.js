48134c933dc8866b257e4af7c1e89f0171951a06
Alan Knowles <alan@roojs.com>
2014-06-11 18:49:18 +0800

    Builder4/WindowLeftTree.bjs Builder4/WindowLeftTree.js Builder4/WindowLeftTree.vala

--- Builder4/WindowLeftTree.js
+++ Builder4/WindowLeftTree.js
@@ -246,7 +246,7 @@ WindowLeftTree=new XObject({
                             
                             
                             
-                            if (selection_text  == path.to_string().substring(0,selection_text.length) {
+                            if (selection_text  == path.to_string().substring(0,selection_text.length)) {
                                 ///print("subpath drag");
                                 if (this.drag_in_motion) {
                                      Gdk.drag_status(ctx, 0 ,time);
--- Builder4/WindowLeftTree.vala
+++ Builder4/WindowLeftTree.vala
@@ -328,7 +328,7 @@ public class Xcls_WindowLeftTree
                         
                         
                         
-                        if (selection_text  == path.to_string().substring(0,selection_text.length) {
+                        if (selection_text  == path.to_string().substring(0,selection_text.length)) {
                             ///print("subpath drag");
                             if (this.drag_in_motion) {
                                  Gdk.drag_status(ctx, 0 ,time);
