e059041790f1058d555036cdfb4e9399b8b96384
Alan Knowles <alan@roojs.com>
2014-06-13 16:38:28 +0800

    Builder4/WindowLeftTree.bjs Builder4/WindowLeftTree.js Builder4/WindowLeftTree.vala

--- Builder4/WindowLeftTree.js
+++ Builder4/WindowLeftTree.js
@@ -838,7 +838,7 @@ WindowLeftTree=new XObject({
                         //print("MOVE NODE");
                         // console.dump(target_data);
                         Gtk.TreeIter old_iter;
-                        var s = this.model.get_selection();
+                        var s = this.get_selection();
                         Gtk.ListStore mod;
                         s.get_selected(out mod , out old_iter);
                         
