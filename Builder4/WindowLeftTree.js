b63c8bbfc005ae6bc5d6103490ef2d1d9952f81e
Alan Knowles <alan@roojs.com>
2014-06-17 16:15:41 +0800

    Builder4/WindowLeftTree.bjs Builder4/WindowLeftTree.js Builder4/WindowLeftTree.vala

--- Builder4/WindowLeftTree.js
+++ Builder4/WindowLeftTree.js
@@ -999,7 +999,7 @@ WindowLeftTree=new XObject({
                     listeners : {
                         activate : () => {
                         
-                             DialogSaveTemplate.show(_this.el.get_parent_window (), _this.model.file.palete(), _this.getActiveElement());
+                             DialogSaveTemplate.show((Gtk.Window) _this.el.get_topLevel (), _this.model.file.palete(), _this.getActiveElement());
                              
                             
                         }
