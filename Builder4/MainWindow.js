0af76cc6755c8b1beb4ce64e5f967450fe42bb94
Alan Knowles <alan@roojs.com>
2014-06-24 20:46:25 +0800

    Test.GtkWriter.vala.c JsRender/Gtk.vala.c JsRender/JsRender.vala.c JsRender/Lang.vala.c JsRender/Node.vala.c JsRender/Roo.vala.c Project/Gtk.vala.c Project/Project.vala.c Project/Roo.vala.c Palete/Gir.vala.c Palete/Gtk.vala.c Palete/Palete.vala.c Builder4/Application.vala.c Builder4/About.vala.c Builder4/DialogConfirm.vala.c Builder4/DialogNewComponent.vala.c Builder4/StandardErrorDialog.vala.c Builder4/DialogSaveTemplate.vala.c Builder4/DialogTemplateSelect.vala.c Builder4/EditProject.vala.c Builder4/Editor.vala.c Builder4/RooProjectProperties.vala.c Builder4/MainWindow.vala.c Builder4/WindowRightPalete.vala.c Builder4/WindowLeftTree.vala.c Builder4/WindowRooView.vala.c Builder4/WindowLeftProps.vala.c

--- Builder4/MainWindow.js
+++ Builder4/MainWindow.js
@@ -70,7 +70,7 @@ MainWindow=new XObject({
                                     id : "clutterembed",
                                     pack : "add",
                                     init : var stage = this.el.get_stage();
-                                    stage.set_background_color(  Color.from_string("#fff"));,
+                                    stage.set_background_color(  Clutter.Color.from_string("#fff"));,
                                     items : [
                                         {
                                             xtype: GtkClutter.Actor,
