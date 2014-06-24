8f851dfff6d28e244c0d34edabfbb99c9d1b814d
Alan Knowles <alan@roojs.com>
2014-06-24 23:37:13 +0800

    Builder4/MainWindow.bjs Builder4/MainWindow.js Builder4/MainWindow.vala

--- Builder4/MainWindow.js
+++ Builder4/MainWindow.js
@@ -110,7 +110,9 @@ MainWindow=new XObject({
                                                     
                                                 //this.el.set_position(100,100);
                                                 this.el.set_pivot_point(0.5f,0.5f);
-                                                this.el.set_size(500,500);
+                                                
+                                                this.el.set_size(_this.clutterembed.get_stage().width-50.,
+                                                        _this.clutterembed.get_stage().height);
                                             }
                                         },
                                         {
--- Builder4/MainWindow.vala
+++ Builder4/MainWindow.vala
@@ -531,7 +531,9 @@ public class Xcls_MainWindow : Object
                     
                 //this.el.set_position(100,100);
                 this.el.set_pivot_point(0.5f,0.5f);
-                this.el.set_size(500,500);
+                
+                this.el.set_size(_this.clutterembed.get_stage().width-50.,
+                        _this.clutterembed.get_stage().height);
             }
         }
 
