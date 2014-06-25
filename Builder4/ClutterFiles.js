bdd60641469741c4d3bef8c51ea67b5e691c0294
Alan Knowles <alan@roojs.com>
2014-06-25 20:22:41 +0800

    Builder4/ClutterFiles.bjs Builder4/ClutterFiles.js Builder4/ClutterFiles.vala

--- Builder4/ClutterFiles.js
+++ Builder4/ClutterFiles.js
@@ -23,7 +23,7 @@ ClutterFiles=new XObject({
                     items : [
                         {
                             xtype: Clutter.Texture,
-                            ctor : "",
+                            ctor : "from_file(fname)",
                             pack : false,
                             x_align : "Clutter.ActorAlign.START",
                             x_expand : "true",
