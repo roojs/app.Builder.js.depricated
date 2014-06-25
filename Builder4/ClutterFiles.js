185fbe96202fba8b1730c6e0694c12ca39059a10
Alan Knowles <alan@roojs.com>
2014-06-26 00:46:24 +0800

    Builder4/ClutterFiles.bjs Builder4/ClutterFiles.js Builder4/ClutterFiles.vala

--- Builder4/ClutterFiles.js
+++ Builder4/ClutterFiles.js
@@ -101,6 +101,7 @@ ClutterFiles=new XObject({
                     pack : false,
                     init : this.el.set_size(100,100);,
                     reactive : true,
+                     : "",
                     items : [
                         {
                             xtype: Clutter.Texture,
