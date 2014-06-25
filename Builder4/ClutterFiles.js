4aa18da340a5d37d0af8afeae7ac9ba9ead2e89b
Alan Knowles <alan@roojs.com>
2014-06-25 19:59:15 +0800

    Builder4/ClutterFiles.bjs Builder4/ClutterFiles.js Builder4/ClutterFiles.vala

--- Builder4/ClutterFiles.js
+++ Builder4/ClutterFiles.js
@@ -20,23 +20,23 @@ ClutterFiles=new XObject({
                 {
                     xtype: Clutter.Actor,
                     pack : false,
-                    items : [
-                        {
-                            xtype: Clutter.Image,
-                            xpack : "",
-                            pack : false
-                        },
-                        {
-                            xtype: Clutter.Text,
-                            pack : false
-                        }
-                    ],
                     layout_manager : {
                         xtype: Clutter.BoxLayout,
                         spacing : 4,
                         xpack : "",
                         init : this.el.set_size(100,100);,
-                        orientation : Clutter.Orientation,VERTICAL
+                        orientation : Clutter.Orientation,VERTICAL,
+                        items : [
+                            {
+                                xtype: Clutter.Image,
+                                xpack : "",
+                                pack : false
+                            },
+                            {
+                                xtype: Clutter.Text,
+                                pack : false
+                            }
+                        ]
                     }
                 }
             ]
