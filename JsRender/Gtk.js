1d627183692ce6f50f1cc9d0289cb65017430d04
Alan Knowles <alan@roojs.com>
2014-06-25 21:05:32 +0800

    JsRender/Gtk.js

--- JsRender/Gtk.js
+++ JsRender/Gtk.js
@@ -552,6 +552,10 @@ Gtk = XObject.define(
                     strbuilder(ipad + "var child_" + i + " = new " + ci.xvala_xcls + "(_this);\n" );
                     strbuilder(ipad + "child_" + i +".ref();\n" ); // we need to reference increase unnamed children...
                     
+                    if (typeof(ci['*prop']) != 'undefined') {
+                        strbuilder(ipad + "this.el." + ci['*prop'] + "= child_" + i + ".el;\n" );
+                        continue
+                    }
                     
                     if (ci.pack === false || ci.pack == "false") {
                         continue;
