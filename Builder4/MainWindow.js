e8b9d2757c85e179e8800dc27a7ea10988dcb226
Alan Knowles <alan@roojs.com>
2014-06-26 00:57:52 +0800

    Builder4/MainWindow.bjs Builder4/MainWindow.js Builder4/MainWindow.vala

--- Builder4/MainWindow.js
+++ Builder4/MainWindow.js
@@ -31,7 +31,7 @@ MainWindow=new XObject({
     destroy : "() => {\n   Gtk.main_quit();\n}",
     id : "MainWindow",
     init : this.el.show_all();,
-    setViewBrowsing : ( ) => {
+    setViewBrowsing : ( )   {
         var el = _this.rooview.el;
         el.save_easing_state();
       
