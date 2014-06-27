cfdc09f3e3937f576fad743bbdeb4bfe7aefc520
Alan Knowles <alan@roojs.com>
2014-06-27 18:32:44 +0800

    Builder4/MainWindow.bjs Builder4/MainWindow.js Builder4/MainWindow.vala Test.GtkWriter.vala.c JsRender/Gtk.vala.c JsRender/JsRender.vala.c JsRender/Lang.vala.c JsRender/Node.vala.c JsRender/Roo.vala.c Project/Gtk.vala.c Project/Project.vala.c Project/Roo.vala.c Palete/Gir.vala.c Palete/Gtk.vala.c Palete/Palete.vala.c Builder4/Application.vala.c Builder4/About.vala.c Builder4/DialogConfirm.vala.c Builder4/DialogNewComponent.vala.c Builder4/StandardErrorDialog.vala.c Builder4/DialogSaveTemplate.vala.c Builder4/DialogTemplateSelect.vala.c Builder4/EditProject.vala.c Builder4/Editor.vala.c Builder4/RooProjectProperties.vala.c Builder4/MainWindow.vala.c Builder4/WindowRightPalete.vala.c Builder4/WindowLeftTree.vala.c Builder4/WindowRooView.vala.c Builder4/WindowLeftProps.vala.c Builder4/ClutterFiles.vala.c Builder4/WindowLeftProjects.vala.c

--- Builder4/MainWindow.js
+++ Builder4/MainWindow.js
@@ -87,7 +87,7 @@ MainWindow=new XObject({
         this.clutterfiles.open.connect((file) => { 
             _this.showViewEditing();
             this.left_tree.model.loadFile(file);
-            this.rooview.loadFile(file);
+            this.window_rooview.loadFile(file);
             print("OPEN : " + file.name);
     
         });
