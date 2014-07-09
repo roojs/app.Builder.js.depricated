c8c16036ae70ac86e2fc5561e1da122cbc894b15
Alan Knowles <alan@roojs.com>
2014-07-09 20:17:23 +0800

    Builder4/ProjectSettings.bjs Builder4/ProjectSettings.js Builder4/ProjectSettings.vala

--- Builder4/ProjectSettings.js
+++ Builder4/ProjectSettings.js
@@ -78,11 +78,11 @@ ProjectSettings=new XObject({
                     listeners : {
                         button_press_event : () => {
                             var buf =    _this.view.el.get_buffer();
-                           Gtk.TextIter s;
-                             Gtk.TextIter e;
+                            Gtk.TextIter s;
+                            Gtk.TextIter e;
                             buf.get_start_iter(out s);
                             buf.get_end_iter(out e);
-                              _this.project.runhtml = buf.get_text(s,e,true);
+                            _this.project.runhtml = buf.get_text(s,e,true);
                             
                                   
                             _this.buttonPressed("save");
