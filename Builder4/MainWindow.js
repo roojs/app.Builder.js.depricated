77bb624df1069b27496865036bfac181ddcb6dd7
Alan Knowles <alan@roojs.com>
2014-07-18 20:29:10 +0800

    Builder4/MainWindow.bjs Builder4/MainWindow.js Builder4/MainWindow.vala

--- Builder4/MainWindow.js
+++ Builder4/MainWindow.js
@@ -514,7 +514,7 @@ MainWindow=new XObject({
                                             _this.projecteditview.el.set_size(alloc.width-50,
                                                    alloc.height / 2.0f);
                                                    
-                                            _this.objectview.el.set_size((alloc.width/2.0f)-50,
+                                            _this.objectview.el.set_size((alloc.width -50)/2.0f,
                                                    alloc.height);
                                             
                                             
--- Builder4/MainWindow.vala
+++ Builder4/MainWindow.vala
@@ -790,7 +790,7 @@ public class Xcls_MainWindow : Object
                 _this.projecteditview.el.set_size(alloc.width-50,
                        alloc.height / 2.0f);
                        
-                _this.objectview.el.set_size((alloc.width/2.0f)-50,
+                _this.objectview.el.set_size((alloc.width -50)/2.0f,
                        alloc.height);
                 
                 
