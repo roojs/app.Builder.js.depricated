8147b892d914d03820e939daaa17aedb27b97863
Alan Knowles <alan@roojs.com>
2014-07-18 19:10:37 +0800

    Builder4/MainWindow.bjs Builder4/MainWindow.js Builder4/MainWindow.vala

--- Builder4/MainWindow.js
+++ Builder4/MainWindow.js
@@ -229,6 +229,31 @@ MainWindow=new XObject({
     },
     'void:showPalete' : () {
     
+         
+         
+         
+         
+         
+        //this.rooview.el.hide();
+        //this.projectsettings.el.show_all();
+        //this.projectsettings.show(this.project);
+    
+        //_this.projecteditview.el.save_easing_state();
+            
+        var el = _this.rooview.el;
+        el.save_easing_state();
+       
+        
+        el.set_scale(0.5f,0.5f);
+    
+        _this.projecteditview.el.set_scale(1.0f,1.0f);
+       
+       
+     
+        //_this.clutterfiles.loadProject(_this.project);
+    
+        el.restore_easing_state();
+        _this.projecteditview.el.restore_easing_state();
         this.state = "palate";
     },
     'void:showProjectEdit' : () {
