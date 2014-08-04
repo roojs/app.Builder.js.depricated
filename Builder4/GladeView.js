a3fe643141564fba1b87f5bf54f4e2281131dbfe
Alan Knowles <alan@roojs.com>
2014-08-04 18:42:52 +0800

    Builder4/GladeView.bjs Builder4/GladeView.js Builder4/GladeView.vala

--- Builder4/GladeView.js
+++ Builder4/GladeView.js
@@ -11,7 +11,8 @@ console = imports.console;
 XObject = imports.XObject.XObject;
 GladeView=new XObject({
     xtype: Glade.DesignView,
-    id : "GladeView"
+    id : "GladeView",
+     : ""
 });
 GladeView.init();
 XObject.cache['/GladeView'] = GladeView;
