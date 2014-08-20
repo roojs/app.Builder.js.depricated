aa69484765f094f60c2a475d91569c6d5b355728
Alan Knowles <alan@roojs.com>
2014-08-20 14:29:22 +0800

    Builder4/WindowLeftTree.bjs Builder4/WindowLeftTree.js Builder4/WindowLeftTree.vala

--- Builder4/WindowLeftTree.js
+++ Builder4/WindowLeftTree.js
@@ -168,6 +168,7 @@ WindowLeftTree=new XObject({
                         // if there are not items in the tree.. the we have to set isOver to true for anything..
                         var isEmpty = false;
                         if (_this.model.el.iter_n_children(null) < 1) {
+                            print("got NO children?\n");
                             isOver = true; //??? 
                             isEmpty = true;
                             pos = Gtk.TreeViewDropPosition.INTO_OR_AFTER;
--- Builder4/WindowLeftTree.vala
+++ Builder4/WindowLeftTree.vala
@@ -288,6 +288,7 @@ public class Xcls_WindowLeftTree : Object
                     // if there are not items in the tree.. the we have to set isOver to true for anything..
                     var isEmpty = false;
                     if (_this.model.el.iter_n_children(null) < 1) {
+                        print("got NO children?\n");
                         isOver = true; //??? 
                         isEmpty = true;
                         pos = Gtk.TreeViewDropPosition.INTO_OR_AFTER;
