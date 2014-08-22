0743466c81535e6ecc35dfe07d5442a4ac805c5a
Alan Knowles <alan@roojs.com>
2014-08-22 12:17:34 +0800

    Builder4/MainWindow.bjs Builder4/MainWindow.js Builder4/MainWindow.vala

--- Builder4/MainWindow.js
+++ Builder4/MainWindow.js
@@ -991,7 +991,7 @@ MainWindow=new XObject({
                                                                     height_request : 50,
                                                                     pack : "false",
                                                                     width_request : 50,
-                                                                    label : "Open\nFiles"
+                                                                    label : "<<"
                                                                 }
                                                             ]
                                                         }
--- Builder4/MainWindow.vala
+++ Builder4/MainWindow.vala
@@ -1503,7 +1503,7 @@ public class Xcls_MainWindow : Object
 
             // set gobject values
             this.el.height_request = 50;
-            this.el.label = "Open\nFiles";
+            this.el.label = "<<";
             this.el.width_request = 50;
 
             // listeners 
