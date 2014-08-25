2d2ce3fe5c90be1ba7c1e245d84800b86d48e3dc
Alan Knowles <alan@roojs.com>
2014-08-25 13:31:26 +0800

    Builder4/ProjectSettings.bjs Builder4/ProjectSettings.js Builder4/ProjectSettings.vala

--- Builder4/ProjectSettings.js
+++ Builder4/ProjectSettings.js
@@ -54,13 +54,7 @@ ProjectSettings=new XObject({
                     xtype: Gtk.Button,
                     listeners : {
                         button_press_event : () => {
-                            var buf =    _this.view.el.get_buffer();
-                           Gtk.TextIter s;
-                             Gtk.TextIter e;
-                            buf.get_start_iter(out s);
-                            buf.get_end_iter(out e);
-                              _this.project.runhtml = buf.get_text(s,e,true);
-                            
+                            _this.save();
                                   
                             _this.buttonPressed("apply");
                                 return false;
@@ -73,13 +67,7 @@ ProjectSettings=new XObject({
                     xtype: Gtk.Button,
                     listeners : {
                         button_press_event : () => {
-                            var buf =    _this.view.el.get_buffer();
-                            Gtk.TextIter s;
-                            Gtk.TextIter e;
-                            buf.get_start_iter(out s);
-                            buf.get_end_iter(out e);
-                            _this.project.runhtml = buf.get_text(s,e,true);
-                            
+                               _this.save();
                                   
                             _this.buttonPressed("save");
                                 return false;
--- Builder4/ProjectSettings.vala
+++ Builder4/ProjectSettings.vala
@@ -151,13 +151,7 @@ public class Xcls_ProjectSettings : Object
 
             // listeners 
             this.el.button_press_event.connect( () => {
-                var buf =    _this.view.el.get_buffer();
-               Gtk.TextIter s;
-                 Gtk.TextIter e;
-                buf.get_start_iter(out s);
-                buf.get_end_iter(out e);
-                  _this.project.runhtml = buf.get_text(s,e,true);
-                
+                _this.save();
                       
                 _this.buttonPressed("apply");
                     return false;
@@ -189,13 +183,7 @@ public class Xcls_ProjectSettings : Object
 
             // listeners 
             this.el.button_press_event.connect( () => {
-                var buf =    _this.view.el.get_buffer();
-                Gtk.TextIter s;
-                Gtk.TextIter e;
-                buf.get_start_iter(out s);
-                buf.get_end_iter(out e);
-                _this.project.runhtml = buf.get_text(s,e,true);
-                
+                   _this.save();
                       
                 _this.buttonPressed("save");
                     return false;
