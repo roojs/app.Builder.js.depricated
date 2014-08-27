a9b9ed62361060f09410278022aaa2c8521e2c76
Alan Knowles <alan@roojs.com>
2014-08-27 14:55:40 +0800

    JsRender/webkit_extension.c JsRender/Gtk.vala.c JsRender/JsRender.vala.c JsRender/Lang.vala.c JsRender/Node.vala.c JsRender/NodeToGlade.vala.c JsRender/NodeToJs.vala.c JsRender/Roo.vala.c Project/Gtk.vala.c Project/Project.vala.c Project/Roo.vala.c Palete/Gir.vala.c Palete/Gtk.vala.c Palete/Palete.vala.c Palete/Roo.vala.c Builder4/Application.vala.c Builder4/About.vala.c Builder4/DialogConfirm.vala.c Builder4/DialogNewComponent.vala.c Builder4/StandardErrorDialog.vala.c Builder4/DialogSaveTemplate.vala.c Builder4/DialogTemplateSelect.vala.c Builder4/EditProject.vala.c Builder4/Editor.vala.c Builder4/ProjectSettings.vala.c Builder4/WindowRightPalete.vala.c Builder4/WindowLeftTree.vala.c Builder4/WindowRooView.vala.c Builder4/WindowLeftProps.vala.c Builder4/ClutterFiles.vala.c Builder4/WindowLeftProjects.vala.c Builder4/WindowAddProp.vala.c Builder4/GladeView.vala.c Builder4/MainWindow.vala.c Builder4/Main.vala.c

--- JsRender/webkit_extension.c
+++ JsRender/webkit_extension.c
@@ -2,7 +2,7 @@
 #include <glib-object.h>
 #include <webkit2/webkit-web-extension.h>
 
-static WebKitWebExtension *glob_extension
+static WebKitWebExtension *glob_extension;
 
 G_MODULE_EXPORT void
 webkit_web_extension_initialize (WebKitWebExtension *extension)
