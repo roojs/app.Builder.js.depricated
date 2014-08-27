c6201b3cf409aa16aa934f2d1e45dcdb0cfb32f3
Alan Knowles <alan@roojs.com>
2014-08-27 15:15:21 +0800

    JsRender/webkit_extension.c

--- JsRender/webkit_extension.c
+++ JsRender/webkit_extension.c
@@ -7,7 +7,7 @@ static WebKitWebExtension *glob_extension;
 G_MODULE_EXPORT void
 webkit_web_extension_initialize (WebKitWebExtension *extension)
 {
-    glob_extension = extension;
+    glob_extension = g_object_ref(extension);
 }
 
 WebKitWebExtension* get_webkit_extension()
