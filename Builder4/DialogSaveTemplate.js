30540f9dcd7a000001daeb002a1a0a094d26636a
Alan Knowles <alan@roojs.com>
2014-09-11 15:27:35 +0800

    Builder4/DialogSaveTemplate.bjs.DL74LX Builder4/DialogSaveTemplate.bjs Builder4/DialogSaveTemplate.js.XC64LX Builder4/DialogSaveTemplate.js Builder4/DialogSaveTemplate.vala.K0U8LX Builder4/DialogSaveTemplate.vala Builder4/DialogSaveTemplate.bjs.BYW3LX Builder4/DialogSaveTemplate.js.TNW3LX Builder4/DialogSaveTemplate.vala.17U1LX Builder4/DialogSaveTemplate.bjs.Q514LX Builder4/DialogSaveTemplate.js.SV04LX Builder4/DialogSaveTemplate.vala.WV34LX

--- Builder4/DialogSaveTemplate.js
+++ Builder4/DialogSaveTemplate.js
@@ -46,18 +46,14 @@ DialogSaveTemplate=new XObject({
                 }
                 break;
             }
-            var f = project.newFile(name);
-            f.tree =  _this.data.deepClone();
-            f.save();
+            this.palete.saveTemplate(name, data);
             
             // now we save it..
             this.el.hide();
             
-            return name;
-     
-     
-     
+            
       
+       
     },
     default_width : 400,
     xtype : "Dialog",
@@ -70,33 +66,6 @@ DialogSaveTemplate=new XObject({
     	delete_event : (self, event) => {
     	       this.el.hide();
     	       return true;
-    	       
-    	   },
-    	response : (self, response_id) => {
-    	   
-    	       if (response_id < 1) {
-    	           this.el.hide();
-    	            return;
-    	       }
-    	       var name = _this.name.el.get_text();
-    	       if (name.length < 1) {
-    	           StandardErrorDialog.singleton().show(
-    	               this.el,
-    	               "You must give the template a name. "
-    	           );
-    	           return;
-    	       }
-    	       if (!Regex.match_simple ("^[A-Za-z]^[A-Za-z0-9 ]+$", name))
-    	       {
-    	           StandardErrorDialog.singleton().show(
-    	               this.el,
-    	               "Template Nane must contain only letters and spaces. "
-    	           );
-    	            return;
-    	       }
-    	       _this.palete.saveTemplate(name, _this.data);
-    	       // now we save it..
-    	       this.el.hide();
     	       
     	   }
     },
--- Builder4/DialogSaveTemplate.vala
+++ Builder4/DialogSaveTemplate.vala
@@ -46,33 +46,6 @@ public class DialogSaveTemplate : Object
             return true;
             
         });
-        this.el.response.connect( (self, response_id) => {
-        
-            if (response_id < 1) {
-                this.el.hide();
-                 return;
-            }
-            var name = _this.name.el.get_text();
-            if (name.length < 1) {
-                StandardErrorDialog.singleton().show(
-                    this.el,
-                    "You must give the template a name. "
-                );
-                return;
-            }
-            if (!Regex.match_simple ("^[A-Za-z]^[A-Za-z0-9 ]+$", name))
-            {
-                StandardErrorDialog.singleton().show(
-                    this.el,
-                    "Template Nane must contain only letters and spaces. "
-                );
-                 return;
-            }
-            _this.palete.saveTemplate(name, _this.data);
-            // now we save it..
-            this.el.hide();
-            
-        });
     }
 
     // user defined functions 
@@ -112,18 +85,14 @@ public class DialogSaveTemplate : Object
                 }
                 break;
             }
-            var f = project.newFile(name);
-            f.tree =  _this.data.deepClone();
-            f.save();
+            this.palete.saveTemplate(name, data);
             
             // now we save it..
             this.el.hide();
             
-            return name;
-     
-     
-     
+            
       
+       
     }
     public class Xcls_HBox2 : Object 
     {
