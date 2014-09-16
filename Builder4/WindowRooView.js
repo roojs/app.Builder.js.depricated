6c47d93a198b387cb59421b9053b03430d114d25
Alan Knowles <alan@roojs.com>
2014-09-16 18:57:32 +0800

    Builder4/WindowRooView.bjs.JU70LX Builder4/WindowRooView.bjs Builder4/WindowRooView.js.9880LX Builder4/WindowRooView.js Builder4/WindowRooView.vala.2MB1LX Builder4/WindowRooView.vala

--- Builder4/WindowRooView.js
+++ Builder4/WindowRooView.js
@@ -279,7 +279,7 @@ WindowRooView=new XObject({
                                     
                                 // force the inspector...        
                                     this.el.get_inspector().show();
-                                    
+                                     this.el.get_inspector().open_window();
                                     
                                     // - no need for this, the builder javascript will call it when build is complete
                                     //GLib.Timeout.add_seconds(1, () => {
