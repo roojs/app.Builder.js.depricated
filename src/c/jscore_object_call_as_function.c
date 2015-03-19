81f45586995d4eafd71dda5f164259eed3123af3
Alan Knowles <alan@roojs.com>
2015-03-19 18:11:35 +0800

    src/c/jscore_object_call_as_function.c

---
+++ src/c/jscore_object_call_as_function.c
@@ -0,0 +1,44 @@
+#include <JavaScriptCore/JavaScript.h>
+#include <glib.h>
+#include <glib-object.h>
+/**
+ a wrapper around call as function, as Value[] arrays do not work in Vala...
+
+*/
+
+
+JSValueRef jscore_object_call_as_function(
+	JSContextRef ctx,
+	JSObjectRef object, 
+	JSObjectRef thisObject,
+	  gchar * val,
+	JSValueRef* exception
+	
+) {
+
+	JSValueRef res;
+    JSValueRef *jsargs;
+    
+    
+    JSStringRef jsstr = JSStringCreateWithUTF8CString (val);
+	JSValueRef valstr = JSValueMakeString (ctx, jsstr);
+	//JSStringRelease (jsstr); //??
+    
+    (JSValueRef *) g_newa (JSValueRef, 1);
+    jsargs[0] =  valstr;
+    
+    res =  JSObjectCallAsFunction(
+			ctx, 
+			object, 
+			thisObject, 
+			1, 
+			jsargs, 
+			exception
+	);
+	// free the args..
+	
+    return res;
+
+  
+}
+  
