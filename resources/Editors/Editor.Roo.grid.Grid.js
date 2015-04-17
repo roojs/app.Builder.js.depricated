e51f1085170a888b04fb84d559f73505a2dbab4e
Alan Knowles <alan@roojs.com>
2015-04-17 17:12:56 +0800

    resources/Editors/Editor.Roo.grid.Grid.bjs resources/Editors/Editor.Roo.grid.Grid.js

--- resources/Editors/Editor.Roo.grid.Grid.js
+++ resources/Editors/Editor.Roo.grid.Grid.js
@@ -215,7 +215,149 @@ Editor.Roo.grid.Grid = new Roo.XComponent({
    tableName : 'core_enum',
    title : _this._strings['c671c787b49f50a3ace9fdc5bd597825'],
    title : _this._strings['c671c787b49f50a3ace9fdc5bd597825'],
-   toBJS : function() { },
+   toBJS : function() { 
+   
+   // convert the selected cells into a BJS data ready to send back to the UI..
+      
+       this.table = "XXX";
+       this.firstTxtCol = "XXX";
+      
+      
+       var jreader = {};
+   
+       JSON.stringify({
+           '|xns' : 'Roo',
+           xtype : "GridPanel",
+           "title": this.table,
+           "fitToframe": true,
+           "fitContainer": true,
+           "tableName": this.table,
+           "background": true,
+           "region" : 'center',
+           "listeners": {
+               "|activate": "function() {\n    _this.panel = this;\n    if (_this.grid) {\n        _this.grid.footer.onClick('first');\n    }\n}"
+           },
+           "items": [
+               {
+                   "*prop": "grid",
+                   "xtype": "Grid",
+                   "autoExpandColumn": this.firstTxtCol,
+                   "loadMask": true,
+                   "listeners": {
+                       "|render": "function() \n" +
+                           "{\n" +
+                           "    _this.grid = this; \n" +
+                           "    //_this.dialog = Pman.Dialog.FILL_IN\n" +
+                           "    if (_this.panel.active) {\n" +
+                           "       this.footer.onClick('first');\n" +
+                           "    }\n" +
+                           "}",
+                       "|rowdblclick": "function (_self, rowIndex, e)\n" + 
+                           "{\n" + 
+                           "    if (!_this.dialog) return;\n" + 
+                           "    _this.dialog.show( this.getDataSource().getAt(rowIndex).data, function() {\n" + 
+                           "        _this.grid.footer.onClick('first');\n" + 
+                           "    }); \n" + 
+                           "}\n"
+                   },
+                   "|xns": "Roo.grid",
+   
+                   "items": [
+                       {
+                           "*prop": "dataSource",
+                           "xtype": "Store",
+                            remoteSort : true,
+                           '|sortInfo' : "{ field : '" + this.firstTxtCol  +  "', direction: 'ASC' }", 
+                           "|xns": "Roo.data",
+                           "items": [
+                               
+                               {
+                                   "*prop": "proxy",
+                                   "xtype": "HttpProxy",
+                                   "method": "GET",
+                                   "|url": "baseURL + '/Roo/" + this.table + ".php'",
+                                   "|xns": "Roo.data"
+                               },
+                               jreader
+                           ]
+                       },
+                       {
+                           "*prop": "footer",
+                           "xtype": "PagingToolbar",
+                           "pageSize": 25,
+                           "displayInfo": true,
+                           "displayMsg": "Displaying " + this.table + "{0} - {1} of {2}",
+                           "emptyMsg": "No " + this.table + " found",
+                           "|xns": "Roo"
+                       },
+                       {
+                           "*prop": "toolbar",
+                           "xtype": "Toolbar",
+                           "|xns": "Roo",
+                           "items": [
+                               {
+                                   "text": "Add",
+                                   "xtype": "Button",
+                                   "cls": "x-btn-text-icon",
+                                   "|icon": "Roo.rootURL + 'images/default/dd/drop-add.gif'",
+                                   "listeners": {
+                                       "|click": "function()\n"+
+                                           "{\n"+
+                                           "    if (!_this.dialog) return;\n" +
+                                           "    _this.dialog.show( { id : 0 } , function() {\n"+
+                                           "        _this.grid.footer.onClick('first');\n"+
+                                           "   }); \n"+
+                                           "}\n"
+                                   },
+                                   "|xns": "Roo.Toolbar"
+                               },
+                               {
+                                   "text": "Edit",
+                                   "xtype": "Button",
+                                   "cls": "x-btn-text-icon",
+                                   "|icon": "Roo.rootURL + 'images/default/tree/leaf.gif'",
+                                   "listeners": {
+                                       "|click": "function()\n"+
+                                           "{\n"+
+                                           "    var s = _this.grid.getSelectionModel().getSelections();\n"+
+                                           "    if (!s.length || (s.length > 1))  {\n"+
+                                           "        Roo.MessageBox.alert(\"Error\", s.length ? \"Select only one Row\" : \"Select a Row\");\n"+
+                                           "        return;\n"+
+                                           "    }\n"+
+                                           "    if (!_this.dialog) return;\n" +
+                                           "    _this.dialog.show(s[0].data, function() {\n"+
+                                           "        _this.grid.footer.onClick('first');\n"+
+                                           "    }); \n"+
+                                           "    \n"+
+                                           "}\n" 
+                                       
+                                   },
+                                   "|xns": "Roo.Toolbar"
+                               },
+                               {
+                                   "text": "Delete",
+                                   "cls": "x-btn-text-icon",
+                                   "|icon": "rootURL + '/Pman/templates/images/trash.gif'",
+                                   "xtype": "Button",
+                                   "listeners": {
+                                       "|click": "function()\n"+
+                                           "{\n"+
+                                           "     Pman.genericDelete(_this, '" + reader.table + "'); \n"+
+                                           "}\n"+
+                                           "        "
+                                   },
+                                   "|xns": "Roo.Toolbar"
+                               }
+                           ]
+                       }, // end toolbar
+                   ].concat( reader.colmodel)
+               }
+           ]
+           
+           
+       }, null, 4)
+   
+   },
    xns : Roo,
    xtype : 'GridPanel',
    listeners : {
