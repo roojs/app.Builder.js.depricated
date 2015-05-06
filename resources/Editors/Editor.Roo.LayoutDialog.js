f2f3d869a0f8be701e815eefb71e6c87b044c079
Edward <edward@roojs.com>
2015-05-06 16:30:39 +0800

    resources/Editors/Editor.Roo.LayoutDialog.bjs resources/Editors/Editor.Roo.LayoutDialog.js

--- resources/Editors/Editor.Roo.LayoutDialog.js
+++ resources/Editors/Editor.Roo.LayoutDialog.js
@@ -489,7 +489,7 @@ Editor.Roo.LayoutDialog = new Roo.XComponent({
    
        var fields = _this.grid.dataSource.reader.recordType.prototype.fields;
        Roo.log('grid!!!!!!!!!!!!!!!!!');
-       Roo.log(_this.grid.dataSource.reader.recordType.prototype.fields);
+       Roo.log(fields);
        var ar = [];
        for (var k in data) { 
            var r = data[k];
