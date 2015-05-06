cd21a4835d2f042566b9af6d2886d05e57449397
Edward <edward@roojs.com>
2015-05-06 12:51:56 +0800

    resources/Editors/Editor.Roo.LayoutDialog.bjs resources/Editors/Editor.Roo.LayoutDialog.js

--- resources/Editors/Editor.Roo.LayoutDialog.js
+++ resources/Editors/Editor.Roo.LayoutDialog.js
@@ -305,8 +305,7 @@ Editor.Roo.LayoutDialog = new Roo.XComponent({
    fitToframe : true,
    loadData : function(data) { 
    
-       //alert("IPC:TEST:" + JSON.stringify(data,null,4));
-       alert("IPC:TEST:" + JSON.stringify({ x: 5, y: 6 },null,4));
+       alert("IPC:TEST:" + JSON.stringify(data,null,4));
        return;
        
        var ar = [];
